9aa599806c2fcbf843063f0036da63bf3a445a79
Daniel Parker <danielaparker@yahoo.com>
2013-07-09 21:38:08 -0400

    Doc src

--- src/jsoncons_test/my_custom_data.hpp
+++ src/jsoncons_test/my_custom_data.hpp
@@ -5,13 +5,12 @@
 #define MY_CUSTOM_DATA_HPP
 
 #include "jsoncons/json.hpp"
-#include "jsoncons/json_serializer.hpp"
 #include <boost/numeric/ublas/matrix.hpp>
 
 namespace jsoncons {
 
-template<> inline
-void serialize(json_out_stream& os, const boost::numeric::ublas::matrix<double>& A)
+template<> 
+inline void serialize(json_out_stream& os, const boost::numeric::ublas::matrix<double>& A)
 {
     os.begin_array();
     for (size_t i = 0; i < A.size1(); ++i)
