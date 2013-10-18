a2b7f8aabcac522c3eaf922046aad3ee76118fa2
Daniel Parker <danielaparker@yahoo.com>
2013-10-18 18:54:50 -0400

    Enhanced exception safety

--- src/jsoncons/json_deserializer.hpp
+++ src/jsoncons/json_deserializer.hpp
@@ -13,6 +13,7 @@
 #include <vector>
 #include <istream>
 #include <cstdlib>
+#include <memory>
 #include "jsoncons/json_char_traits.hpp"
 #include "jsoncons/json1.hpp"
 #include "jsoncons/json_input_handler.hpp"
@@ -32,11 +33,9 @@ class basic_json_deserializer : public basic_json_input_handler<Char>
             {
             case object_t:
                 structure_.object_ = new json_object<Char>();
-                //structure_.object_->reserve(4);
                 break;
             case array_t:
                 structure_.array_ =  new json_array<Char>();
-                //structure_.array_->reserve(4);
                 break;
             }
         }
@@ -58,6 +57,32 @@ class basic_json_deserializer : public basic_json_input_handler<Char>
     };
 
 public:
+    ~basic_json_deserializer()
+    {
+        if (stack_.size() > 0)
+        {
+            // Abnormal termination, clean up stack
+            for (size_t i = 0; i < stack_.size(); ++i)
+            {
+                try
+                {
+                    switch (stack_[i].type_)
+                    {
+                    case object_t:
+                        delete stack_[i].structure_.object_;
+                        break;
+                    case array_t:
+                        delete stack_[i].structure_.array_;
+                        break;
+                    }
+                }
+                catch (...)
+                {
+                }
+            }
+        }
+    }
+
     virtual void begin_json()
     {
     }
@@ -73,25 +98,25 @@ public:
 
     virtual void end_object(const basic_parsing_context<Char>& context)
     {
-		json_object<Char>* var = stack_.back().structure_.object_;
+		std::unique_ptr<json_object<Char>> var = std::unique_ptr<json_object<Char>>(stack_.back().structure_.object_);
         var->sort_members();
 		stack_.pop_back();
         if (stack_.size() > 0)
         {
             if (stack_.back().is_object())
             {
-                stack_.back().pair_.second = basic_json<Char>(var);	    
+                stack_.back().pair_.second = basic_json<Char>(var.release());	    
                 stack_.back().structure_.object_->push_back(std::pair<std::basic_string<Char>,basic_json<Char>>(std::move(stack_.back().pair_)));
             }
             else
             {
-                basic_json<Char> val(var);	    
+                basic_json<Char> val(var.release());	    
                 stack_.back().structure_.array_->push_back(std::move(val));
             }
         }
         else
         {
-            basic_json<Char> val(var);	    
+            basic_json<Char> val(var.release());	    
             root_ = std::move(val);
         }
     }
