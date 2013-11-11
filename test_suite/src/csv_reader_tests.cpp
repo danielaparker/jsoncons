eb1b0d29f38d3d9efa127298a99bb9aae8e0dc31
Daniel Parker <danielaparker@yahoo.com>
2013-11-11 14:25:29 -0500

    csv_serializer

--- test_suite/src/csv_reader_tests.cpp
+++ test_suite/src/csv_reader_tests.cpp
@@ -4,6 +4,7 @@
 #include <boost/test/unit_test.hpp>
 #include <boost/numeric/ublas/matrix.hpp>
 #include "jsoncons_ext/csv/csv_reader.hpp"
+#include "jsoncons_ext/csv/csv_serializer.hpp"
 #include "jsoncons/json_reader.hpp"
 #include <sstream>
 #include <vector>
@@ -11,13 +12,14 @@
 #include <ctime>
 
 using jsoncons::json_deserializer;
+using jsoncons_ext::csv::csv_serializer;
 using jsoncons::json;
 using jsoncons_ext::csv::csv_reader;
 using jsoncons::json_reader;
 using jsoncons::pretty_print;
 using std::string;
 
-BOOST_AUTO_TEST_CASE(test_comma_delimited_file)
+BOOST_AUTO_TEST_CASE(read_comma_delimited_file)
 {
     std::cout << "###test_csv_reader" << std::endl;
 
@@ -31,6 +33,21 @@ BOOST_AUTO_TEST_CASE(test_comma_delimited_file)
     json countries = std::move(handler.root());
 
     std::cout << pretty_print(countries) << std::endl;
+} 
+
+BOOST_AUTO_TEST_CASE(serialize_comma_delimited_file)
+{
+    std::string in_file = "input/countries.json";
+    std::ifstream is(in_file);
+
+    json_deserializer handler;
+    json_reader reader(is,handler);
+    reader.read();
+    json countries = std::move(handler.root());
+
+    csv_serializer serializer(std::cout);
+
+    countries.to_stream(serializer);
 } 
 
 BOOST_AUTO_TEST_CASE(test_tab_delimited_file)
