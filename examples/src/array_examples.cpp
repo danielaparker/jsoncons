8d4f55aaa7b90d4eb29296f84c36fa7a2aa79746
Marc Chevrier <marc.chevrier@gmail.com>
2014-01-31 10:05:31 +0100

    add template method make_array

--- examples/src/array_examples.cpp
+++ examples/src/array_examples.cpp
@@ -94,7 +94,7 @@ void make_empty_array()
 void make_1_dimensional_array_1()
 {
     std::cout << "1 dimensional array 1" <<std::endl;
-    json a = json::make_array();
+    json a = json::make_array<1>();
     a.resize_array(10,0);
     a[1] = 1;
     a[2] = 2;
@@ -104,7 +104,7 @@ void make_1_dimensional_array_1()
 void make_1_dimensional_array_2()
 { 
     std::cout << "1 dimensional array 2" <<std::endl;
-    json a = json::make_array(10,0);
+    json a = json::make_array<1>(10,0);
     a[1] = 1;
     a[2] = 2;
     std::cout << pretty_print(a) << std::endl;
@@ -113,7 +113,7 @@ void make_1_dimensional_array_2()
 void make_2_dimensional_array()
 {
     std::cout << "2 dimensional array" <<std::endl;
-    json a = json::make_2d_array(3,4,0);
+    json a = json::make_array<2>(3,4,0);
     a[0][0] = "Tenor";
     a[0][1] = "ATM vol";
     a[0][2] = "25-d-MS"; 
@@ -133,7 +133,7 @@ void make_2_dimensional_array()
 void make_3_dimensional_array()
 {
     std::cout << "3 dimensional array" <<std::endl;
-    json a = json::make_3d_array(4,3,2,0);
+    json a = json::make_array<3>(4,3,2,0);
     a[0][2][0] = 2;
     a[0][2][1] = 3;
     std::cout << pretty_print(a) << std::endl;
