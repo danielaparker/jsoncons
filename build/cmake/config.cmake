5c8d998b0c93e42199cc3c0dfa9ad09a30a870ce
Marc Chevrier <marc.chevrier@gmail.com>
2014-02-10 12:47:34 +0100

    Gcc is now default compiler on Linux

--- build/cmake/config.cmake
+++ build/cmake/config.cmake
@@ -1,9 +1,6 @@
 
 # set compiler on some platforms
-if (UNIX)
-  if (NOT CMAKE_C_COMPILER)
-	set (CMAKE_C_COMPILER clang)
-  endif()
+if (APPLE)
   if (NOT CMAKE_CXX_COMPILER)
 	set (CMAKE_CXX_COMPILER clang++)
   endif()
