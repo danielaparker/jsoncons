1bfa17416dc403459c3b6808ed09a0d221c8d050
Daniel Parker <danielaparker@yahoo.com>
2015-02-24 21:24:00 -0500

    Reorganized header files, removed include of json2.hpp in json_serializer.hpp and csv_serializer.hpp

--- src/jsoncons/output_format.hpp
+++ src/jsoncons/output_format.hpp
@@ -168,6 +168,105 @@ private:
     bool escape_solidus_;
 };
 
+template<typename Char>
+void escape_string(const Char* s,
+                   size_t length,
+                   const basic_output_format<Char>& format,
+                   std::basic_ostream<Char>& os)
+{
+    const Char* begin = s;
+    const Char* end = s + length;
+    for (const Char* it = begin; it != end; ++it)
+    {
+        Char c = *it;
+        switch (c)
+        {
+        case '\\':
+            os << '\\' << '\\';
+            break;
+        case '"':
+            os << '\\' << '\"';
+            break;
+        case '\b':
+            os << '\\' << 'b';
+            break;
+        case '\f':
+            os << '\\';
+            os << 'f';
+            break;
+        case '\n':
+            os << '\\';
+            os << 'n';
+            break;
+        case '\r':
+            os << '\\';
+            os << 'r';
+            break;
+        case '\t':
+            os << '\\';
+            os << 't';
+            break;
+        default:
+            uint32_t u(c >= 0 ? c : 256 + c);
+            if (format.escape_solidus() && c == '/')
+            {
+                os << '\\';
+                os << '/';
+            }
+            else if (is_control_character(u) || format.escape_all_non_ascii())
+            {
+                // convert utf8 to codepoint
+                uint32_t cp = json_char_traits<Char, sizeof(Char)>::convert_char_to_codepoint(it, end);
+                if (is_non_ascii_character(cp) || is_control_character(u))
+                {
+                    if (cp > 0xFFFF)
+                    {
+                        cp -= 0x10000;
+                        uint32_t first = (cp >> 10) + 0xD800;
+                        uint32_t second = ((cp & 0x03FF) + 0xDC00);
+
+                        os << '\\';
+                        os << 'u';
+                        os << to_hex_character(first >> 12 & 0x000F);
+                        os << to_hex_character(first >> 8  & 0x000F);
+                        os << to_hex_character(first >> 4  & 0x000F);
+                        os << to_hex_character(first     & 0x000F);
+                        os << '\\';
+                        os << 'u';
+                        os << to_hex_character(second >> 12 & 0x000F);
+                        os << to_hex_character(second >> 8  & 0x000F);
+                        os << to_hex_character(second >> 4  & 0x000F);
+                        os << to_hex_character(second     & 0x000F);
+                    }
+                    else
+                    {
+                        os << '\\';
+                        os << 'u';
+                        os << to_hex_character(cp >> 12 & 0x000F);
+                        os << to_hex_character(cp >> 8  & 0x000F);
+                        os << to_hex_character(cp >> 4  & 0x000F);
+                        os << to_hex_character(cp     & 0x000F);
+                    }
+                }
+                else
+                {
+                    os << c;
+                }
+            }
+            else if (format.escape_solidus() && c == '/')
+            {
+                os << '\\';
+                os << '/';
+            }
+            else
+            {
+                os << c;
+            }
+            break;
+        }
+    }
+}
+
 typedef basic_output_format<char> output_format;
 typedef basic_output_format<wchar_t> woutput_format;
 
