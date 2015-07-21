fbfd118bfa0f383c0af5182018f1b3b984c74685
Daniel Parker <danielaparker@yahoo.com>
2015-07-20 23:33:41 -0400

    New parser

--- src/jsoncons/parse_error_handler.hpp
+++ src/jsoncons/parse_error_handler.hpp
@@ -160,6 +160,10 @@ namespace json_parser_errc
 {
     enum json_parser_errc_t 
     {
+        unexpected_eof,
+        invalid_json_text,
+        extra_character,
+        max_depth_exceeded,
         mismatched_parentheses_or_brackets,
         single_quote,
         illegal_character_in_string,
@@ -173,15 +177,10 @@ namespace json_parser_errc
         illegal_control_character,
         illegal_escaped_character,
         expected_codepoint_surrogate_pair,
-        invalid_codepoint_surrogate_pair,
         invalid_hex_escape_sequence,
         invalid_unicode_escape_sequence,
         leading_zero,
-        invalid_number,
-        unexpected_eof,
-        invalid_json_text,
-        extra_character,
-        max_depth_exceeded
+        invalid_number
     };
 }
 
@@ -197,6 +196,14 @@ public:
     {
         switch (ev)
         {
+        case json_parser_errc::unexpected_eof:
+            return "Unexpected end of file";
+        case json_parser_errc::invalid_json_text:
+            return "A jSON text must be an object or array";
+        case json_parser_errc::extra_character:
+            return "Unexpected non-whitespace character after JSON text";
+        case json_parser_errc::max_depth_exceeded:
+            return "Maximum stack depth exceeded";
         case json_parser_errc::mismatched_parentheses_or_brackets:
             return "Mismatched parentheses or brackets";
         case json_parser_errc::single_quote:
@@ -223,8 +230,6 @@ public:
             return "Illegal escaped character in string";
         case json_parser_errc::expected_codepoint_surrogate_pair:
             return "Invalid codepoint, expected another \\u token to begin the second half of a codepoint surrogate pair.";
-        case json_parser_errc::invalid_codepoint_surrogate_pair:
-            return "Invalid codepoint, expected another \\u token to begin the second half of a codepoint surrogate pair.";
         case json_parser_errc::invalid_hex_escape_sequence:
             return "Invalid codepoint, expected hexadecimal digit.";
         case json_parser_errc::invalid_unicode_escape_sequence:
@@ -233,14 +238,6 @@ public:
             return "Invalid number";
         case json_parser_errc::leading_zero:
             return "A number cannot have a leading zero";
-        case json_parser_errc::unexpected_eof:
-            return "Unexpected end of file";
-        case json_parser_errc::invalid_json_text:
-            return "A jSON text must be an object or array";
-        case json_parser_errc::extra_character:
-            return "Unexpected non-whitespace character after JSON text";
-        case json_parser_errc::max_depth_exceeded:
-            return "Too deep";
         default:
             return "Unknown JSON parser error";
         }
