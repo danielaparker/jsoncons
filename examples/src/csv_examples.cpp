c475e4281bd32b67b56435369cd23197e7317788
Daniel Parker <danielaparker@yahoo.com>
2015-10-21 00:10:49 -0400

    Added support for data types in csv_reader

--- examples/src/csv_examples.cpp
+++ examples/src/csv_examples.cpp
@@ -3,14 +3,31 @@
 
 #include <string>
 #include "jsoncons/json.hpp"
+#include "jsoncons_ext/csv/csv_reader.hpp"
 
 using jsoncons::json;
 using jsoncons::pretty_print;
 using jsoncons::output_format;
+using jsoncons::json_deserializer;
+using jsoncons::csv::csv_reader;
+using jsoncons::csv::csv_parameters;
 using std::string;
 
 void read_csv_file()
 {
+    string text = "name,lname,age\nxyz,p,20\nabc,q,25";
+    std::istringstream is(text);
+
+    json_deserializer handler;
+
+    csv_parameters params;
+    params.assume_header(true);
+    params.data_types("string,string,integer");
+    csv_reader reader(is,handler,params);
+    reader.read();
+    json val = std::move(handler.root());
+
+    std::cout << pretty_print(val) << std::endl;
 }
 
 void write_csv_file()
