d5d352b9e9e945d4e7c86543e2ba86e200187649
Daniel Parker <danielaparker@yahoo.com>
2015-11-24 13:13:46 -0500

    Implements start,end,step

--- test_suite/src/jsonpath_tests.cpp
+++ test_suite/src/jsonpath_tests.cpp
@@ -44,7 +44,6 @@ BOOST_AUTO_TEST_CASE(test_jsonpath_1b)
         std::cout << pretty_print(result[i]) << std::endl;
     }
 }
-#endif
 BOOST_AUTO_TEST_CASE(test_jsonpath_1b)
 {
     const char* text = "{ \"store\": {\"book\": [ { \"category\": \"reference\",\"author\": \"Nigel Rees\",\"title\": \"Sayings of the Century\",\"price\": 8.95},{ \"category\": \"fiction\",\"author\": \"Evelyn Waugh\",\"title\": \"Sword of Honour\",\"price\": 12.99},{ \"category\": \"fiction\",\"author\": \"Herman Melville\",\"title\": \"Moby Dick\",\"isbn\": \"0-553-21311-3\",\"price\": 8.99},{ \"category\": \"fiction\",\"author\": \"J. R. R. Tolkien\",\"title\": \"The Lord of the Rings\",\"isbn\": \"0-395-19395-8\",\"price\": 22.99}],\"bicycle\": {\"color\": \"red\",\"price\": 19.95}}}";
@@ -57,7 +56,6 @@ BOOST_AUTO_TEST_CASE(test_jsonpath_1b)
         std::cout << pretty_print(result[i]) << std::endl;
     }
 }
-#if 0
 BOOST_AUTO_TEST_CASE(test_jsonpath_1c)
 {
     const char* text = "{ \"store\": {\"book\": [ { \"category\": \"reference\",\"author\": \"Nigel Rees\",\"title\": \"Sayings of the Century\",\"price\": 8.95},{ \"category\": \"fiction\",\"author\": \"Evelyn Waugh\",\"title\": \"Sword of Honour\",\"price\": 12.99},{ \"category\": \"fiction\",\"author\": \"Herman Melville\",\"title\": \"Moby Dick\",\"isbn\": \"0-553-21311-3\",\"price\": 8.99},{ \"category\": \"fiction\",\"author\": \"J. R. R. Tolkien\",\"title\": \"The Lord of the Rings\",\"isbn\": \"0-395-19395-8\",\"price\": 22.99}],\"bicycle\": {\"color\": \"red\",\"price\": 19.95}}}";
@@ -116,3 +114,20 @@ BOOST_AUTO_TEST_CASE(test_jsonpath_alternative_indices)
     }
 }
 #endif
+
+BOOST_AUTO_TEST_CASE(test_jsonpath_start_end)
+{
+    const char* text = "{ \"store\": {\"book\": [ { \"category\": \"reference\",\"author\": \"Nigel Rees\",\"title\": \"Sayings of the Century\",\"price\": 8.95},{ \"category\": \"fiction\",\"author\": \"Evelyn Waugh\",\"title\": \"Sword of Honour\",\"price\": 12.99},{ \"category\": \"fiction\",\"author\": \"Herman Melville\",\"title\": \"Moby Dick\",\"isbn\": \"0-553-21311-3\",\"price\": 8.99},{ \"category\": \"fiction\",\"author\": \"J. R. R. Tolkien\",\"title\": \"The Lord of the Rings\",\"isbn\": \"0-395-19395-8\",\"price\": 22.99}],\"bicycle\": {\"color\": \"red\",\"price\": 19.95}}}";
+
+    json val = json::parse_string(text);
+
+    std::string q = "$..book[:2]";
+    std::vector<json> result = jsonpath_query(val,q);
+
+    std::cout << q << std::endl;
+    for (size_t i = 0; i < result.size(); ++i)
+    {
+        std::cout << pretty_print(result[i]) << std::endl;
+    }
+}
+
