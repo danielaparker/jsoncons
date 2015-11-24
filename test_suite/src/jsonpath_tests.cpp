879f283b0fa6a7c470fa169bb89d5c5bbd68e53d
Daniel Parker <danielaparker@yahoo.com>
2015-11-23 21:57:58 -0500

    Implemented alternative array indices

--- test_suite/src/jsonpath_tests.cpp
+++ test_suite/src/jsonpath_tests.cpp
@@ -18,16 +18,19 @@ using jsoncons::jsonpath::jsonpath_query;
 using jsoncons::pretty_print;
 using jsoncons::json;
 
+#if 0
 BOOST_AUTO_TEST_CASE(test_jsonpath)
 {
     const char* text = "{ \"store\": {\"book\": [ { \"category\": \"reference\",\"author\": \"Nigel Rees\",\"title\": \"Sayings of the Century\",\"price\": 8.95},{ \"category\": \"fiction\",\"author\": \"Evelyn Waugh\",\"title\": \"Sword of Honour\",\"price\": 12.99},{ \"category\": \"fiction\",\"author\": \"Herman Melville\",\"title\": \"Moby Dick\",\"isbn\": \"0-553-21311-3\",\"price\": 8.99},{ \"category\": \"fiction\",\"author\": \"J. R. R. Tolkien\",\"title\": \"The Lord of the Rings\",\"isbn\": \"0-395-19395-8\",\"price\": 22.99}],\"bicycle\": {\"color\": \"red\",\"price\": 19.95}}}";
 
     json val = json::parse_string(text);
 
-	//json result = jsonpath_query(val,"$.store.book");
+    std::vector<json> result = jsonpath_query(val,"$.store.book");
 
-    //std::cout << pretty_print(result) << std::endl;
-    //BOOST_CHECK(result.is_array());
+    for (size_t i = 0; i < result.size(); ++i)
+    {
+        std::cout << pretty_print(result[i]) << std::endl;
+    }
 }
 
 BOOST_AUTO_TEST_CASE(test_jsonpath_1b)
@@ -36,22 +39,24 @@ BOOST_AUTO_TEST_CASE(test_jsonpath_1b)
 
     json val = json::parse_string(text);
 
-//	json result = jsonpath_query(val,"$['store']['book']");
-
-//    std::cout << pretty_print(result) << std::endl;
-//    BOOST_CHECK(result.is_array());
+    std::vector<json> result = jsonpath_query(val,"$['store']['book']");
+    for (size_t i = 0; i < result.size(); ++i)
+    {
+        std::cout << pretty_print(result[i]) << std::endl;
+    }
 }
 
-BOOST_AUTO_TEST_CASE(test_jsonpath_1b)
+BOOST_AUTO_TEST_CASE(test_jsonpath_1c)
 {
     const char* text = "{ \"store\": {\"book\": [ { \"category\": \"reference\",\"author\": \"Nigel Rees\",\"title\": \"Sayings of the Century\",\"price\": 8.95},{ \"category\": \"fiction\",\"author\": \"Evelyn Waugh\",\"title\": \"Sword of Honour\",\"price\": 12.99},{ \"category\": \"fiction\",\"author\": \"Herman Melville\",\"title\": \"Moby Dick\",\"isbn\": \"0-553-21311-3\",\"price\": 8.99},{ \"category\": \"fiction\",\"author\": \"J. R. R. Tolkien\",\"title\": \"The Lord of the Rings\",\"isbn\": \"0-395-19395-8\",\"price\": 22.99}],\"bicycle\": {\"color\": \"red\",\"price\": 19.95}}}";
 
     json val = json::parse_string(text);
 
-	json result = jsonpath_query(val,"$['store']['book'][*]");
-
-    std::cout << pretty_print(result) << std::endl;
-    BOOST_CHECK(result.is_array());
+    std::vector<json> result = jsonpath_query(val,"$['store']['book'][*]");
+    for (size_t i = 0; i < result.size(); ++i)
+    {
+        std::cout << pretty_print(result[i]) << std::endl;
+    }
 }
 
 BOOST_AUTO_TEST_CASE(test_jsonpath2)
@@ -60,8 +65,42 @@ BOOST_AUTO_TEST_CASE(test_jsonpath2)
 
     json val = json::parse_string(text);
 
-	//json result = jsonpath_query(val,"$.store.book[0]");
+    std::vector<json> result = jsonpath_query(val,"$.store.book[0]");
+    for (size_t i = 0; i < result.size(); ++i)
+    {
+        std::cout << pretty_print(result[i]) << std::endl;
+    }
+}
 
-    //std::cout << pretty_print(result) << std::endl;
-    //BOOST_CHECK(result.is_array());
+BOOST_AUTO_TEST_CASE(test_jsonpath_recursive_descent)
+{
+    const char* text = "{ \"store\": {\"book\": [ { \"category\": \"reference\",\"author\": \"Nigel Rees\",\"title\": \"Sayings of the Century\",\"price\": 8.95},{ \"category\": \"fiction\",\"author\": \"Evelyn Waugh\",\"title\": \"Sword of Honour\",\"price\": 12.99},{ \"category\": \"fiction\",\"author\": \"Herman Melville\",\"title\": \"Moby Dick\",\"isbn\": \"0-553-21311-3\",\"price\": 8.99},{ \"category\": \"fiction\",\"author\": \"J. R. R. Tolkien\",\"title\": \"The Lord of the Rings\",\"isbn\": \"0-395-19395-8\",\"price\": 22.99}],\"bicycle\": {\"color\": \"red\",\"price\": 19.95}}}";
+
+    json val = json::parse_string(text);
+
+    std::string q = "$..book[2]";
+    std::vector<json> result = jsonpath_query(val,q);
+
+    std::cout << q << std::endl;
+    for (size_t i = 0; i < result.size(); ++i)
+    {
+        std::cout << pretty_print(result[i]) << std::endl;
+    }
+}
+#endif
+
+BOOST_AUTO_TEST_CASE(test_jsonpath_alternative_indices)
+{
+    const char* text = "{ \"store\": {\"book\": [ { \"category\": \"reference\",\"author\": \"Nigel Rees\",\"title\": \"Sayings of the Century\",\"price\": 8.95},{ \"category\": \"fiction\",\"author\": \"Evelyn Waugh\",\"title\": \"Sword of Honour\",\"price\": 12.99},{ \"category\": \"fiction\",\"author\": \"Herman Melville\",\"title\": \"Moby Dick\",\"isbn\": \"0-553-21311-3\",\"price\": 8.99},{ \"category\": \"fiction\",\"author\": \"J. R. R. Tolkien\",\"title\": \"The Lord of the Rings\",\"isbn\": \"0-395-19395-8\",\"price\": 22.99}],\"bicycle\": {\"color\": \"red\",\"price\": 19.95}}}";
+
+    json val = json::parse_string(text);
+
+    std::string q = "$..book[0,1]";
+    std::vector<json> result = jsonpath_query(val,q);
+
+    std::cout << q << std::endl;
+    for (size_t i = 0; i < result.size(); ++i)
+    {
+        std::cout << pretty_print(result[i]) << std::endl;
+    }
 }
