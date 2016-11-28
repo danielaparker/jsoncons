f6002485a7ecb4286e3a8b3c68df9bc75055e858
danielaparker <danielaparker@yahoo.com>
2016-11-28 11:19:31 -0500

    Fix for gcc

--- src/jsoncons/jsoncons_io.hpp
+++ src/jsoncons/jsoncons_io.hpp
@@ -377,7 +377,7 @@ public:
     {
     }
 
-    void operator()(double val, uint8_t precision, buffered_output<CharT>& os)
+    void operator()(double val, uint8_t precision, buffered_output<CharT>& os) const
     {
         char buf[_CVTBUFSIZE];
         int decimal_point = 0;
@@ -487,7 +487,7 @@ public:
         oss_.imbue(std::locale::classic());
         oss_.precision(precision);
     }
-    void print(double val, uint8_t precision, buffered_output<CharT>& os)
+    void operator()(double val, uint8_t precision, buffered_output<CharT>& os) const
     {
         oss_.clear_sequence();
         oss_.precision((precision == 0) ? precision_ : precision);
