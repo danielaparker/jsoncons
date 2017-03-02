77c1a180e3cee0f1a8aa4ad40572ac7e4c12188d
danielaparker <danielaparker@yahoo.com>
2017-03-01 23:06:08 -0500

    testing

--- src/jsoncons/json_reader.hpp
+++ src/jsoncons/json_reader.hpp
@@ -92,10 +92,13 @@ public:
         parser_.reset();
         while (!eof_ && !parser_.done())
         {
+            std::cout << "Check 10" << std::endl;
             if (parser_.source_exhausted())
             {
+                std::cout << "Check 20" << std::endl;
                 if (!is_.eof())
                 {
+                    std::cout << "Check 30" << std::endl;
                     is_.read(buffer_.data(), buffer_capacity_);
                     buffer_length_ = static_cast<size_t>(is_.gcount());
                     parser_.set_buffer(buffer_.data(),buffer_length_);
@@ -116,6 +119,7 @@ public:
             }
             if (!eof_)
             {
+                std::cout << "Check 40" << std::endl;
                 parser_.parse_some();
             }
         }
