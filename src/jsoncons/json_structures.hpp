6c6f4e49999fd4136f0954aa3a6da7dab2d0b206
danielaparker <danielaparker@yahoo.com>
2017-08-13 22:36:55 -0400

    Code cleanup

--- src/jsoncons/json_structures.hpp
+++ src/jsoncons/json_structures.hpp
@@ -4,8 +4,8 @@
 
 // See https://github.com/danielaparker/jsoncons for latest version
 
-#ifndef JSONCONS_JSON_CONTAINER_HPP
-#define JSONCONS_JSON_CONTAINER_HPP
+#ifndef JSONCONS_JSON_STRUCTURES_HPP
+#define JSONCONS_JSON_STRUCTURES_HPP
 
 #include <string>
 #include <vector>
