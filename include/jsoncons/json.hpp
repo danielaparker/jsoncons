eba1f294736240924614f27de741ce5a3e35c114
danielaparker <danielaparker@yahoo.com>
2018-01-23 13:00:58 -0500

    Improved key_value_pair

--- include/jsoncons/json.hpp
+++ include/jsoncons/json.hpp
@@ -2886,7 +2886,7 @@ public:
     }
 
     template <class T>
-    explicit basic_json(const T& val)
+    basic_json(const T& val)
         : var_(json_type_traits<basic_json,T>::to_json(val).var_)
     {
     }
