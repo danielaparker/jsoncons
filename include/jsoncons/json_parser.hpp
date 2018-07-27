be123d025efa8246312dd948e1b4611de7a1c88b
danielaparker <danielaparker@yahoo.com>
2018-07-27 09:21:14 -0400

    json_parser - use delegating constructors

--- include/jsoncons/json_parser.hpp
+++ include/jsoncons/json_parser.hpp
@@ -175,128 +175,42 @@ class basic_json_parser : private serializing_context
     basic_json_parser& operator=(const basic_json_parser&) = delete;
 
 public:
-
     basic_json_parser()
-       : handler_(default_content_handler_),
-         err_handler_(default_err_handler_),
-         cp_(0),
-         cp2_(0),
-         precision_(0), 
-         decimal_places_(0), 
-         line_(1),
-         column_(1),
-         nesting_depth_(0), 
-         initial_stack_capacity_(default_initial_stack_capacity_),
-         begin_input_(nullptr),
-         input_end_(nullptr),
-         input_ptr_(nullptr),
-         state_(parse_state::start)
+        : basic_json_parser(default_content_handler_, basic_json_serializing_options<CharT>(), default_err_handler_)
     {
-        string_buffer_.reserve(initial_string_buffer_capacity_);
-        number_buffer_.reserve(initial_number_buffer_capacity_);
-        max_nesting_depth_ = (std::numeric_limits<size_t>::max)();
-
-        state_stack_.reserve(initial_stack_capacity_);
-        push_state(parse_state::root);
     }
 
     basic_json_parser(parse_error_handler& err_handler)
-       : handler_(default_content_handler_),
-         err_handler_(err_handler),
-         cp_(0),
-         cp2_(0),
-         precision_(0), 
-         decimal_places_(0), 
-         line_(1),
-         column_(1),
-         nesting_depth_(0), 
-         initial_stack_capacity_(default_initial_stack_capacity_),
-         begin_input_(nullptr),
-         input_end_(nullptr),
-         input_ptr_(nullptr),
-         state_(parse_state::start)
+        : basic_json_parser(default_content_handler_, basic_json_serializing_options<CharT>(), err_handler)
     {
-        string_buffer_.reserve(initial_string_buffer_capacity_);
-        number_buffer_.reserve(initial_number_buffer_capacity_);
-        max_nesting_depth_ = (std::numeric_limits<size_t>::max)();
-
-        state_stack_.reserve(initial_stack_capacity_);
-        push_state(parse_state::root);
     }
 
     basic_json_parser(basic_json_content_handler<CharT>& handler)
-       : handler_(handler),
-         err_handler_(default_err_handler_),
-         cp_(0),
-         cp2_(0),
-         precision_(0), 
-         decimal_places_(0), 
-         line_(1),
-         column_(1),
-         nesting_depth_(0), 
-         initial_stack_capacity_(default_initial_stack_capacity_),
-         begin_input_(nullptr),
-         input_end_(nullptr),
-         input_ptr_(nullptr),
-         state_(parse_state::start)
+        : basic_json_parser(handler, basic_json_serializing_options<CharT>(), default_err_handler_)
     {
-        string_buffer_.reserve(initial_string_buffer_capacity_);
-        number_buffer_.reserve(initial_number_buffer_capacity_);
-        max_nesting_depth_ = (std::numeric_limits<size_t>::max)();
-
-        state_stack_.reserve(initial_stack_capacity_);
-        push_state(parse_state::root);
     }
 
     basic_json_parser(basic_json_content_handler<CharT>& handler,
                       parse_error_handler& err_handler)
-       : handler_(handler),
-         err_handler_(err_handler),
-         cp_(0),
-         cp2_(0),
-         precision_(0), 
-         decimal_places_(0), 
-         line_(1),
-         column_(1),
-         nesting_depth_(0), 
-         initial_stack_capacity_(default_initial_stack_capacity_),
-         begin_input_(nullptr),
-         input_end_(nullptr),
-         input_ptr_(nullptr),
-         state_(parse_state::start)
+        : basic_json_parser(handler, basic_json_serializing_options<CharT>(), err_handler)
     {
-        string_buffer_.reserve(initial_string_buffer_capacity_);
-        number_buffer_.reserve(initial_number_buffer_capacity_);
-        max_nesting_depth_ = (std::numeric_limits<size_t>::max)();
+    }
 
-        state_stack_.reserve(initial_stack_capacity_);
-        push_state(parse_state::root);
+    basic_json_parser(const basic_json_serializing_options<CharT>& options)
+        : basic_json_parser(default_content_handler_, options, default_err_handler_)
+    {
+    }
+
+    basic_json_parser(const basic_json_serializing_options<CharT>& options, 
+                      parse_error_handler& err_handler)
+        : basic_json_parser(default_content_handler_, options, err_handler)
+    {
     }
 
     basic_json_parser(basic_json_content_handler<CharT>& handler,
                       const basic_json_serializing_options<CharT>& options)
-       : replacement_filter_(handler,options),
-         handler_((options.can_read_nan_replacement() || options.can_read_pos_inf_replacement() || options.can_read_neg_inf_replacement()) ? replacement_filter_ : handler),
-         err_handler_(default_err_handler_),
-         cp_(0),
-         cp2_(0),
-         precision_(0), 
-         decimal_places_(0), 
-         line_(1),
-         column_(1),
-         nesting_depth_(0), 
-         initial_stack_capacity_(default_initial_stack_capacity_),
-         begin_input_(nullptr),
-         input_end_(nullptr),
-         input_ptr_(nullptr),
-         state_(parse_state::start)
+        : basic_json_parser(handler, options, default_err_handler_)
     {
-        string_buffer_.reserve(initial_string_buffer_capacity_);
-        number_buffer_.reserve(initial_number_buffer_capacity_);
-        max_nesting_depth_ = options.max_nesting_depth();
-
-        state_stack_.reserve(initial_stack_capacity_);
-        push_state(parse_state::root);
     }
 
     basic_json_parser(basic_json_content_handler<CharT>& handler, 
