daf6d93835d0d60da11b7168de843e9625ef34da
danielaparker <danielaparker@yahoo.com>
2018-08-01 22:29:35 -0400

    Added byte_string_tag

--- include/jsoncons/json.hpp
+++ include/jsoncons/json.hpp
@@ -788,7 +788,7 @@ public:
             }
         }
 
-        variant(const basic_bignum<byte_allocator_type>& bs, const Allocator& allocator)
+        variant(const basic_bignum<byte_allocator_type>& n, const Allocator& allocator)
         {
             int signum;
             std::vector<uint8_t> data;
