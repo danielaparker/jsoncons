5b37e78b5ec2f995e5a352fc1834ca8520ff81f9
danielaparker <danielaparker@yahoo.com>
2018-09-13 17:55:48 -0400

    test

--- build/cmake/Linux.cmake
+++ build/cmake/Linux.cmake
@@ -8,16 +8,9 @@
 
 # customize compiler flags
 ## Add new flags
-# add_definitions (-std=c++11 -pthread)
-# if (${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
-#   add_definitions (-stdlib=libc++)
-# endif()
+add_definitions (-pthread)
 
 set (CMAKE_SHARED_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -pthread")
 set (CMAKE_MODULE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -pthread")
 set (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -pthread")
-# if (${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
-#  set (CMAKE_SHARED_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libc++")
-#  set (CMAKE_MODULE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libc++")
-#  set (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libc++")
-# endif()
+
