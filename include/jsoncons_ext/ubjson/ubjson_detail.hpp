38b35ff0a7c87d21d774f04dccb1c133c7622571
danielaparker <danielaparker@yahoo.com>
2018-12-06 17:13:38 -0500

    Added ubjson format codes

--- include/jsoncons_ext/ubjson/ubjson_detail.hpp
+++ include/jsoncons_ext/ubjson/ubjson_detail.hpp
@@ -21,6 +21,27 @@ namespace jsoncons { namespace ubjson {
 
 namespace ubjson_format
 {
+    const uint8_t null_type = 'Z';
+    const uint8_t no_op_type = 'N';
+    const uint8_t true_type = 'T';
+    const uint8_t false_type = 'F';
+    const uint8_t int8_type = 'i';
+    const uint8_t uint8_type = 'U';
+    const uint8_t int16_type = 'I';
+    const uint8_t int32_type = 'l';
+    const uint8_t int64_type = 'L';
+    const uint8_t float32_type = 'd';
+    const uint8_t float64_type = 'D';
+    const uint8_t high_precision_number_type = 'H';
+    const uint8_t char_type = 'C';
+    const uint8_t string_type = 'S';
+    const uint8_t start_array_marker = '[';
+    const uint8_t end_array_marker = ']';
+    const uint8_t start_object_marker = '{';
+    const uint8_t end_object_marker = '}';
+    const uint8_t type_marker = '$';
+    const uint8_t count_marker = '#';
+
     const uint8_t positive_fixint_base_cd = 0x00;
     const uint8_t nil_cd = 0xc0;
     const uint8_t false_cd = 0xc2;
