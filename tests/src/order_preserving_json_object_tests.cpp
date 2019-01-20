f7516540b699f4c537aaa5ce220e5793900457bf
danielaparker <danielaparker@yahoo.com>
2019-01-19 21:57:35 -0500

    json_object insert key and value

--- tests/src/order_preserving_json_object_tests.cpp
+++ tests/src/order_preserving_json_object_tests.cpp
@@ -28,7 +28,8 @@ TEST_CASE("order preserving insert")
     items.emplace_back("a", 7);
 
     o.insert(std::make_move_iterator(items.begin()), std::make_move_iterator(items.end()), 
-             [](item_type&& item){return ojson::key_value_type(std::move(item.first),std::move(item.second));});
+             [](item_type&& item){return std::move(item.first);},
+             [](item_type&& item) {return std::move(item.second); });
 
     SECTION("iterate")
     {
