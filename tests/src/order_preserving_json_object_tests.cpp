ab97243a5fde7a3ae662b082e1b239072d5da138
danielaparker <danielaparker@yahoo.com>
2019-01-20 17:19:07 -0500

    insert for objects

--- tests/src/order_preserving_json_object_tests.cpp
+++ tests/src/order_preserving_json_object_tests.cpp
@@ -28,8 +28,7 @@ TEST_CASE("order preserving insert")
     items.emplace_back("a", 7);
 
     o.insert(std::make_move_iterator(items.begin()), std::make_move_iterator(items.end()), 
-             [](item_type&& item){return std::move(item.first);},
-             [](item_type&& item) {return std::move(item.second); });
+             [](item_type&& item){return ojson::key_value_type(std::forward<ojson::key_type>(item.first),std::forward<ojson>(item.second));});
 
     SECTION("iterate")
     {
