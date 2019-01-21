444779c559115079906539a6ba3b1d3398b93f93
danielaparker <danielaparker@yahoo.com>
2019-01-21 12:48:30 -0500

    test compile

--- tests/src/tests_main.cpp
+++ tests/src/tests_main.cpp
@@ -1,3 +1,15 @@
 #define CATCH_CONFIG_MAIN
 #include <catch/catch.hpp>
 
+#include <string> 
+
+std::string func(std::string s) { 
+    return s; 
+} 
+
+int main(int argc, char **argv) { 
+  std::string s1 = func(s1); 
+  std::string s2(s2); 
+  std::string s3 = s3; 
+  return 0; 
+} 
