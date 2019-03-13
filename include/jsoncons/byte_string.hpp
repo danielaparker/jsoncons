6f39f887cc19b99628c33050e84fe911baee31a0
danielaparker <danielaparker@yahoo.com>
2019-03-12 22:56:36 -0400

    Support as_byte_string for json string

--- include/jsoncons/byte_string.hpp
+++ include/jsoncons/byte_string.hpp
@@ -19,10 +19,127 @@
 #include <jsoncons/config/jsoncons_config.hpp>
 #include <jsoncons/json_exception.hpp>
 
-namespace jsoncons
-{
+namespace jsoncons {
 
 // Algorithms
+
+namespace detail {
+
+    template <class Container>
+    size_t encode_base64_generic(const uint8_t* first, size_t length, const char alphabet[65], Container& result)
+    {
+        size_t count = 0;
+        const uint8_t* last = first + length;
+        unsigned char a3[3];
+        unsigned char a4[4];
+        unsigned char fill = alphabet[64];
+        int i = 0;
+        int j = 0;
+
+        while (first != last)
+        {
+            a3[i++] = *first++;
+            if (i == 3)
+            {
+                a4[0] = (a3[0] & 0xfc) >> 2;
+                a4[1] = ((a3[0] & 0x03) << 4) + ((a3[1] & 0xf0) >> 4);
+                a4[2] = ((a3[1] & 0x0f) << 2) + ((a3[2] & 0xc0) >> 6);
+                a4[3] = a3[2] & 0x3f;
+
+                for (i = 0; i < 4; i++) 
+                {
+                    result.push_back(alphabet[a4[i]]);
+                    ++count;
+                }
+                i = 0;
+            }
+        }
+
+        if (i > 0)
+        {
+            for (j = i; j < 3; ++j) 
+            {
+                a3[j] = 0;
+            }
+
+            a4[0] = (a3[0] & 0xfc) >> 2;
+            a4[1] = ((a3[0] & 0x03) << 4) + ((a3[1] & 0xf0) >> 4);
+            a4[2] = ((a3[1] & 0x0f) << 2) + ((a3[2] & 0xc0) >> 6);
+
+            for (j = 0; j < i + 1; ++j) 
+            {
+                result.push_back(alphabet[a4[j]]);
+                ++count;
+            }
+
+            if (fill != 0)
+            {
+                while (i++ < 3) 
+                {
+                    result.push_back(fill);
+                    ++count;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    template <class InputIt, class F, class Container>
+    typename std::enable_if<sizeof(typename Container::value_type) == sizeof(uint8_t),void>::type 
+    decode_base64_generic(InputIt first, InputIt last, 
+                          const uint8_t reverse_alphabet[256],
+                          F f,
+                          Container& result)
+    {
+        uint8_t a4[4], a3[3];
+        uint8_t i = 0;
+        uint8_t j = 0;
+
+        while (first != last && *first != '=')
+        {
+            if (!(*first >= 0 && *first < 128) || !f((int)*first))
+            {
+                JSONCONS_THROW(json_runtime_error<std::invalid_argument>("Cannot decode encoded byte string"));
+            }
+
+            a4[i++] = *first++; 
+            if (i == 4)
+            {
+                for (i = 0; i < 4; ++i) 
+                {
+                    a4[i] = reverse_alphabet[a4[i]];
+                }
+
+                a3[0] = (a4[0] << 2) + ((a4[1] & 0x30) >> 4);
+                a3[1] = ((a4[1] & 0xf) << 4) + ((a4[2] & 0x3c) >> 2);
+                a3[2] = ((a4[2] & 0x3) << 6) +   a4[3];
+
+                for (i = 0; i < 3; i++) 
+                {
+                    result.push_back(a3[i]);
+                }
+                i = 0;
+            }
+        }
+
+        if (i > 0)
+        {
+            for (j = 0; j < i; ++j) 
+            {
+                a4[j] = reverse_alphabet[a4[j]];
+            }
+
+            a3[0] = (a4[0] << 2) + ((a4[1] & 0x30) >> 4);
+            a3[1] = ((a4[1] & 0xf) << 4) + ((a4[2] & 0x3c) >> 2);
+
+            for (j = 0; j < i - 1; ++j) 
+            {
+                result.push_back(a3[j]);
+            }
+        }
+    }
+}
 
 template <class Container>
 size_t encode_base16(const uint8_t* data, size_t length, Container& result)
@@ -39,73 +156,13 @@ size_t encode_base16(const uint8_t* data, size_t length, Container& result)
 }
 
 template <class Container>
-size_t encode_base64_generic(const uint8_t* first, size_t length, const char alphabet[65], Container& result)
-{
-    size_t count = 0;
-    const uint8_t* last = first + length;
-    unsigned char a3[3];
-    unsigned char a4[4];
-    unsigned char fill = alphabet[64];
-    int i = 0;
-    int j = 0;
-
-    while (first != last)
-    {
-        a3[i++] = *first++;
-        if (i == 3)
-        {
-            a4[0] = (a3[0] & 0xfc) >> 2;
-            a4[1] = ((a3[0] & 0x03) << 4) + ((a3[1] & 0xf0) >> 4);
-            a4[2] = ((a3[1] & 0x0f) << 2) + ((a3[2] & 0xc0) >> 6);
-            a4[3] = a3[2] & 0x3f;
-
-            for (i = 0; i < 4; i++) 
-            {
-                result.push_back(alphabet[a4[i]]);
-                ++count;
-            }
-            i = 0;
-        }
-    }
-
-    if (i > 0)
-    {
-        for (j = i; j < 3; ++j) 
-        {
-            a3[j] = 0;
-        }
-
-        a4[0] = (a3[0] & 0xfc) >> 2;
-        a4[1] = ((a3[0] & 0x03) << 4) + ((a3[1] & 0xf0) >> 4);
-        a4[2] = ((a3[1] & 0x0f) << 2) + ((a3[2] & 0xc0) >> 6);
-
-        for (j = 0; j < i + 1; ++j) 
-        {
-            result.push_back(alphabet[a4[j]]);
-            ++count;
-        }
-
-        if (fill != 0)
-        {
-            while (i++ < 3) 
-            {
-                result.push_back(fill);
-                ++count;
-            }
-        }
-    }
-
-    return count;
-}
-
-template <class Container>
 size_t encode_base64url(const uint8_t* first, size_t length, Container& result)
 {
     static constexpr char alphabet[65] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
                                                   "abcdefghijklmnopqrstuvwxyz"
                                                   "0123456789-_"
                                                   "\0";
-    return encode_base64_generic(first, length, alphabet, result);
+    return detail::encode_base64_generic(first, length, alphabet, result);
 }
 
 template <class Container>
@@ -115,7 +172,7 @@ size_t encode_base64(const uint8_t* first, size_t length, Container& result)
                                                "abcdefghijklmnopqrstuvwxyz"
                                                "0123456789+/"
                                                "=";
-    return encode_base64_generic(first, length, alphabet, result);
+    return detail::encode_base64_generic(first, length, alphabet, result);
 }
 
 inline 
@@ -131,60 +188,6 @@ static bool is_base64url(int c)
 }
 
 // decode
-template <class InputIt, class F, class Container>
-typename std::enable_if<sizeof(typename Container::value_type) == sizeof(uint8_t),void>::type 
-decode_base64_generic(InputIt first, InputIt last, 
-                      const uint8_t reverse_alphabet[256],
-                      F f,
-                      Container& result)
-{
-    uint8_t a4[4], a3[3];
-    uint8_t i = 0;
-    uint8_t j = 0;
-
-    while (first != last && *first != '=')
-    {
-        if (!(*first >= 0 && *first < 128) || !f((int)*first))
-        {
-            JSONCONS_THROW(json_runtime_error<std::invalid_argument>("Cannot decode encoded byte string"));
-        }
-
-        a4[i++] = *first++; 
-        if (i == 4)
-        {
-            for (i = 0; i < 4; ++i) 
-            {
-                a4[i] = reverse_alphabet[a4[i]];
-            }
-
-            a3[0] = (a4[0] << 2) + ((a4[1] & 0x30) >> 4);
-            a3[1] = ((a4[1] & 0xf) << 4) + ((a4[2] & 0x3c) >> 2);
-            a3[2] = ((a4[2] & 0x3) << 6) +   a4[3];
-
-            for (i = 0; i < 3; i++) 
-            {
-                result.push_back(a3[i]);
-            }
-            i = 0;
-        }
-    }
-
-    if (i > 0)
-    {
-        for (j = 0; j < i; ++j) 
-        {
-            a4[j] = reverse_alphabet[a4[j]];
-        }
-
-        a3[0] = (a4[0] << 2) + ((a4[1] & 0x30) >> 4);
-        a3[1] = ((a4[1] & 0xf) << 4) + ((a4[2] & 0x3c) >> 2);
-
-        for (j = 0; j < i - 1; ++j) 
-        {
-            result.push_back(a3[j]);
-        }
-    }
-}
 
 template <class InputIt, class Container>
 typename std::enable_if<sizeof(typename Container::value_type) == sizeof(uint8_t),void>::type 
@@ -208,7 +211,7 @@ decode_base64url(InputIt first, InputIt last, Container& result)
         0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
         0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
     };
-    decode_base64_generic(first, last, reverse_alphabet, is_base64url, result);
+    jsoncons::detail::decode_base64_generic(first, last, reverse_alphabet, is_base64url, result);
 }
 
 template <class InputIt, class Container>
@@ -233,7 +236,7 @@ decode_base64(InputIt first, InputIt last, Container& result)
         0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
         0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
     };
-    decode_base64_generic(first, last, reverse_alphabet, is_base64, result);
+    jsoncons::detail::decode_base64_generic(first, last, reverse_alphabet, is_base64, result);
 }
 
 template <class InputIt,class Container>
