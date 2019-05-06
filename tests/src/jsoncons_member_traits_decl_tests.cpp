618457b08d8ca2840b30a339357887708ebff045
danielaparker <danielaparker@yahoo.com>
2019-05-05 22:43:05 -0400

    armhf tests

--- tests/src/jsoncons_member_traits_decl_tests.cpp
+++ tests/src/jsoncons_member_traits_decl_tests.cpp
@@ -36,7 +36,7 @@ namespace jsoncons_member_traits_decl_tests {
 namespace ns = jsoncons_member_traits_decl_tests;
 
 JSONCONS_MEMBER_TRAITS_DECL(ns::book,author,title,price)
-JSONCONS_MEMBER_TRAITS_DECL(ns::book2,author,title,price,isbn);
+JSONCONS_MEMBER_TRAITS_DECL(ns::reputation_object, application, reputons)
 
 TEST_CASE("JSONCONS_MEMBER_TRAITS_DECL tests")
 {
