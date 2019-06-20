469e065840593134627789e8ac6ba37edf03a212
danielaparker <danielaparker@yahoo.com>
2019-06-19 21:20:25 -0400

    Source cleanup

--- tests/src/staj_iterator_tests.cpp
+++ tests/src/staj_iterator_tests.cpp
@@ -41,17 +41,21 @@ TEST_CASE("array_iterator test")
     ]
     )";
 
-    std::istringstream is(s);
-
-    json_cursor reader(is);
+    json_cursor reader(s);
 
     staj_array_iterator<json> it(reader);
+    staj_array_iterator<json> end;
 
-    for (const auto& val : it)
-    {
-        std::cout << val << "\n";
-    }
-
+    const auto& j1 = *it;
+    CHECK(j1["firstName"].as<std::string>() == std::string("Tom"));
+    ++it;
+    const auto& j2 = *it;
+    CHECK(j2["firstName"].as<std::string>() == std::string("Catherine"));
+    ++it;
+    const auto& j3 = *it;
+    CHECK(j3["firstName"].as<std::string>() == std::string("William"));
+    ++it;
+    CHECK((it == end));
 }
 
 TEST_CASE("object_iterator test")
@@ -66,15 +70,23 @@ TEST_CASE("object_iterator test")
     )";
 
     std::istringstream is(s);
-
     json_cursor reader(is);
-
     staj_object_iterator<json> it(reader);
+    staj_object_iterator<json> end;
 
-    for (const auto& item : it)
-    {
-        std::cout << item.first << ": " << item.second << "\n";
-    }
+    const auto& p1 = *it;
+    CHECK(p1.second.as<int>() == 100);
+    ++it;
+    const auto& p2 = *it;
+    CHECK(p2.second.as<std::string>() == std::string("Tom"));
+    ++it;
+    const auto& p3 = *it;
+    CHECK(p3.second.as<std::string>() == std::string("Cochrane"));
+    ++it;
+    const auto& p4 = *it;
+    CHECK(p4.second.as<int>() == 55);
+    ++it;
+    CHECK((it == end));
 }
 
 
