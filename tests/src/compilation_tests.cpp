cc7024b04d4a624ea0376cfb8adbed77e710b5e1
danielaparker <danielaparker@yahoo.com>
2019-06-19 20:53:45 -0400

    compilation tests

---
+++ tests/src/compilation_tests.cpp
@@ -0,0 +1,16 @@
+// Copyright 2019 Daniel Parker
+// Distributed under Boost license
+
+template <typename T>
+class string {
+};
+#include <jsoncons/json.hpp>
+#include <jsoncons_ext/bson/bson.hpp>
+#include <jsoncons_ext/cbor/cbor.hpp>
+#include <jsoncons_ext/csv/csv.hpp>
+#include <jsoncons_ext/msgpack/msgpack.hpp>
+#include <jsoncons_ext/jsonpointer/jsonpointer.hpp>
+#include <jsoncons_ext/jsonpatch/jsonpatch.hpp>
+#include <jsoncons_ext/jsonpath/json_query.hpp>
+#include <jsoncons_ext/ubjson/ubjson.hpp>
+
