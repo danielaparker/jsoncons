54509c66befaf73de7ea9877ee15e5d362ffaa6b
danielaparker <danielaparker@yahoo.com>
2019-07-11 15:51:27 -0400

    string_type -> key_type in basic_json

--- tests/src/order_preserving_json_object_tests.cpp
+++ tests/src/order_preserving_json_object_tests.cpp
@@ -15,7 +15,7 @@ using namespace jsoncons;
 
 TEST_CASE("json sorted_unique_range_tag")
 {
-    typedef std::pair<json::string_type, json> item_type;
+    typedef std::pair<json::key_type, json> item_type;
     std::vector<item_type> items;
     items.emplace_back("a", 1);
     items.emplace_back("b", 2);
@@ -71,7 +71,7 @@ TEST_CASE("json sorted_unique_range_tag")
 
 TEST_CASE("ojson sorted_unique_range_tag")
 {
-    typedef std::pair<ojson::string_type, ojson> item_type;
+    typedef std::pair<ojson::key_type, ojson> item_type;
     std::vector<item_type> items;
     items.emplace_back("a", 1);
     items.emplace_back("b", 2);
@@ -129,7 +129,7 @@ TEST_CASE("order preserving insert")
 {
     json_object<std::string, ojson> o;
 
-    typedef std::pair<ojson::string_type,ojson> item_type;
+    typedef std::pair<ojson::key_type,ojson> item_type;
     std::vector<item_type> items;
     items.emplace_back("b", 1);
     items.emplace_back("a", 2);
@@ -140,7 +140,7 @@ TEST_CASE("order preserving insert")
     items.emplace_back("a", 7);
 
     o.insert(std::make_move_iterator(items.begin()), std::make_move_iterator(items.end()), 
-             [](item_type&& item){return ojson::key_value_type(std::forward<ojson::string_type>(item.first),std::forward<ojson>(item.second));});
+             [](item_type&& item){return ojson::key_value_type(std::forward<ojson::key_type>(item.first),std::forward<ojson>(item.second));});
 
     SECTION("iterate")
     {
