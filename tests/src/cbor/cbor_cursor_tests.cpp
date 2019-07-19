f076a6e82af75e96afbf52daf4db9896763e7cd1
danielaparker <danielaparker@yahoo.com>
2019-07-19 13:46:32 -0400

    Fix cbor_cursor

--- tests/src/cbor/cbor_cursor_tests.cpp
+++ tests/src/cbor/cbor_cursor_tests.cpp
@@ -76,63 +76,51 @@ TEST_CASE("cbor_cursor reputon test")
         CHECK(cursor.done());
     }
 }
-#if 0
-TEST_CASE("cbor_cursor readme test")
+
+TEST_CASE("cbor_cursor indefinite array of array test")
 {
     std::vector<uint8_t> data = {0x82,0x83,0x63,0x66,0x6f,0x6f,0x44,0x50,0x75,0x73,0x73,0xc3,0x49,0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x83,0x63,0x62,0x61,0x72,0xd6,0x44,0x50,0x75,0x73,0x73,0xc4,0x82,0x21,0x19,0x6a,0xb3};
 
     SECTION("test 1")
     {
-        std::cout << "cbor_cursor tests 2\n";
         cbor::cbor_bytes_cursor cursor(data);
-        for (; !cursor.done(); cursor.next())
-        {
-            const auto& event = cursor.current();
-            switch (event.event_type())
-            {
-                case staj_event_type::begin_array:
-                    std::cout << event.event_type() << " " << "(" << event.tag() << ")\n";
-                    break;
-                case staj_event_type::end_array:
-                    std::cout << event.event_type() << " " << "(" << event.tag() << ")\n";
-                    break;
-                case staj_event_type::begin_object:
-                    std::cout << event.event_type() << " " << "(" << event.tag() << ")\n";
-                    break;
-                case staj_event_type::end_object:
-                    std::cout << event.event_type() << " " << "(" << event.tag() << ")\n";
-                    break;
-                case staj_event_type::name:
-                    // Or std::string_view, if supported
-                    std::cout << event.event_type() << ": " << event.get<jsoncons::string_view>() << " " << "(" << event.tag() << ")\n";
-                    break;
-                case staj_event_type::string_value:
-                    // Or std::string_view, if supported
-                    std::cout << event.event_type() << ": " << event.get<jsoncons::string_view>() << " " << "(" << event.tag() << ")\n";
-                    break;
-                case staj_event_type::byte_string_value:
-                    std::cout << event.event_type() << ": " << event.get<jsoncons::byte_string_view>() << " " << "(" << event.tag() << ")\n";
-                    break;
-                case staj_event_type::null_value:
-                    std::cout << event.event_type() << " " << "(" << event.tag() << ")\n";
-                    break;
-                case staj_event_type::bool_value:
-                    std::cout << event.event_type() << ": " << std::boolalpha << event.get<bool>() << " " << "(" << event.tag() << ")\n";
-                    break;
-                case staj_event_type::int64_value:
-                    std::cout << event.event_type() << ": " << event.get<int64_t>() << " " << "(" << event.tag() << ")\n";
-                    break;
-                case staj_event_type::uint64_value:
-                    std::cout << event.event_type() << ": " << event.get<uint64_t>() << " " << "(" << event.tag() << ")\n";
-                    break;
-                case staj_event_type::double_value:
-                    std::cout << event.event_type() << ": "  << event.get<double>() << " " << "(" << event.tag() << ")\n";
-                    break;
-                default:
-                    std::cout << "Unhandled event type " << event.event_type() << " " << "(" << event.tag() << ")\n";
-                    break;
-            }
-        }
+        CHECK(cursor.current().event_type() == staj_event_type::begin_array);
+        CHECK(cursor.current().tag() == semantic_tag::none);
+        cursor.next();
+        CHECK(cursor.current().event_type() == staj_event_type::begin_array);
+        CHECK(cursor.current().tag() == semantic_tag::none);
+        cursor.next();
+        CHECK(cursor.current().event_type() == staj_event_type::string_value);
+        CHECK(cursor.current().tag() == semantic_tag::none);
+        cursor.next();
+        CHECK(cursor.current().event_type() == staj_event_type::byte_string_value);
+        CHECK(cursor.current().tag() == semantic_tag::none);
+        cursor.next();
+        CHECK(cursor.current().event_type() == staj_event_type::string_value);
+        CHECK(cursor.current().tag() == semantic_tag::bigint);
+        cursor.next();
+        CHECK(cursor.current().event_type() == staj_event_type::end_array);
+        CHECK(cursor.current().tag() == semantic_tag::none);
+        cursor.next();
+        CHECK(cursor.current().event_type() == staj_event_type::begin_array);
+        CHECK(cursor.current().tag() == semantic_tag::none);
+        cursor.next();
+        CHECK(cursor.current().event_type() == staj_event_type::string_value);
+        CHECK(cursor.current().tag() == semantic_tag::none);
+        cursor.next();
+        CHECK(cursor.current().event_type() == staj_event_type::byte_string_value);
+        CHECK(cursor.current().tag() == semantic_tag::base64);
+        cursor.next();
+        CHECK(cursor.current().event_type() == staj_event_type::string_value);
+        CHECK(cursor.current().tag() == semantic_tag::bigdec);
+        cursor.next();
+        CHECK(cursor.current().event_type() == staj_event_type::end_array);
+        CHECK(cursor.current().tag() == semantic_tag::none);
+        cursor.next();
+        CHECK(cursor.current().event_type() == staj_event_type::end_array);
+        CHECK(cursor.current().tag() == semantic_tag::none);
+        cursor.next();
+        CHECK(cursor.done());
     }
 }
-#endif
+
