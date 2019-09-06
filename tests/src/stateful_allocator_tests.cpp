493a89f64586c6e9c5a34a9f427377a63af30d02
Daniel Parker <danielaparker@yahoo.com>
2019-09-06 07:22:07 -0400

    Statefull allocator tests

--- tests/src/stateful_allocator_tests.cpp
+++ tests/src/stateful_allocator_tests.cpp
@@ -12,6 +12,97 @@
 
 using namespace jsoncons;
 
+// http://coliru.stacked-crooked.com/a/cfd0c5c5021596ad
+
+template <typename T>
+class FreelistAllocator {
+    union node {
+        node* next;
+        typename std::aligned_storage<sizeof(T), alignof(T)>::type storage;
+    };
+
+    node* list = nullptr;
+
+    void clear() noexcept {
+        auto p = list;
+        while (p) {
+            auto tmp = p;
+            p = p->next;
+            delete tmp;
+        }
+        list = nullptr;
+    }
+
+public:
+    using value_type = T;
+    using size_type = std::size_t;
+    using propagate_on_container_move_assignment = std::true_type;
+
+    FreelistAllocator() noexcept = default;
+    FreelistAllocator(const FreelistAllocator&) noexcept {}
+    template <typename U>
+    FreelistAllocator(const FreelistAllocator<U>&) noexcept {}
+    FreelistAllocator(FreelistAllocator&& other) noexcept :  list(other.list) {
+        other.list = nullptr;
+    }
+
+    FreelistAllocator& operator = (const FreelistAllocator&) noexcept {
+        // noop
+        return *this;
+    }
+
+    FreelistAllocator& operator = (FreelistAllocator&& other) noexcept {
+        clear();
+        list = other.list;
+        other.list = nullptr;
+        return *this;
+    }
+
+    ~FreelistAllocator() noexcept { clear(); }
+
+    T* allocate(size_type n) {
+        std::cout << "Allocate(" << n << ") from ";
+        if (n == 1) {
+            auto ptr = list;
+            if (ptr) {
+                std::cout << "freelist\n";
+                list = list->next;
+            } else {
+                std::cout << "new node\n";
+                ptr = new node;
+            }
+            return reinterpret_cast<T*>(ptr);
+        }
+
+        std::cout << "::operator new\n";
+        return static_cast<T*>(::operator new(n * sizeof(T)));
+    }
+
+    void deallocate(T* ptr, size_type n) noexcept {
+        std::cout << "Deallocate(" << static_cast<void*>(ptr) << ", " << n << ") to ";
+        if (n == 1) {
+            std::cout << "freelist\n";
+            auto node_ptr = reinterpret_cast<node*>(ptr);
+            node_ptr->next = list;
+            list = node_ptr;
+        } else {
+            std::cout << "::operator delete\n";
+            ::operator delete(ptr);
+        }
+    }
+};
+
+template <typename T, typename U>
+inline bool operator == (const FreelistAllocator<T>&, const FreelistAllocator<U>&) {
+    return true;
+}
+
+template <typename T, typename U>
+inline bool operator != (const FreelistAllocator<T>&, const FreelistAllocator<U>&) {
+    return false;
+}
+
+/*
 class pool 
 { 
 private: 
@@ -166,15 +257,16 @@ bool operator!=(const pool_allocator<T> &s0, const pool_allocator<T> &s1)
 {
     return s0.pool_ptr_ != s1.pool_ptr_;
 }
+*/
+
 #if !defined(__GNUC__) 
 // gcc 4.8 basic_string doesn't satisfy C++11 allocator requirements
 TEST_CASE("test_string_allocation")
 {
 
-    pool a_pool(1024);
-    pool_allocator<json> allocator(&a_pool); 
+    FreelistAllocator<char> allocator; 
 
-    typedef basic_json<char,sorted_policy,pool_allocator<json>> myjson;
+    typedef basic_json<char,sorted_policy,FreelistAllocator<char>> myjson;
 
     SECTION("construct")
     {
@@ -185,8 +277,6 @@ TEST_CASE("test_string_allocation")
         //          << ", construct count = " << a_pool.construct_count_ 
         //          << ", destroy count = " << a_pool.destroy_count_ 
         //          << ", deallocate count = " << a_pool.deallocate_count_ << std::endl;
-        CHECK(a_pool.allocate_count_ == a_pool.deallocate_count_);
-        CHECK(a_pool.construct_count_ == a_pool.destroy_count_);
     }
 
     SECTION("construct")
@@ -200,10 +290,12 @@ TEST_CASE("test_string_allocation")
 
     SECTION("parse")
     {
+        FreelistAllocator<char> allocator2; 
+
         std::string s = "String too long for short string";
         std::string input = "\"" + s + "\"";
 
-        json_decoder<myjson> decoder(allocator);
+        json_decoder<myjson,FreelistAllocator<char>> decoder(allocator, allocator2);
         try
         {
             json_reader reader(input,decoder);
