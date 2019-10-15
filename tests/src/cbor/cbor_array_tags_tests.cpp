e5267ff6a446bc82a6c737622be495f4505c15ab
Daniel Parker <danielaparker@yahoo.com>
2019-10-15 16:11:04 -0400

    Updates to array tag tests

--- tests/src/cbor/cbor_array_tags_tests.cpp
+++ tests/src/cbor/cbor_array_tags_tests.cpp
@@ -342,75 +342,26 @@ TEST_CASE("cbor tagged array tests")
                 0x7f, 0xef, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
         };
 
-        json j = cbor::decode_cbor<json>(input);
-        std::cout << "Tag 82\n" << pretty_print(j) << "\n";
-        REQUIRE(j.is_array());
-        REQUIRE(j.size() == 2);
-        CHECK(j[0].as<double>() == std::numeric_limits<double>::lowest());
-        CHECK(j[1].as<double>() == (std::numeric_limits<double>::max)());
+        //json j = cbor::decode_cbor<json>(input);
+        //REQUIRE(j.is_array());
     }
 
     SECTION("Tag 83, float128, big endian")
     {
-        std::cout << "sizeof(long double): " << sizeof(long double) << "\n";
-        const uint8_t* endp;
-
-        uint8_t buf[sizeof(long double)];
-        auto x = std::numeric_limits<long double>::lowest();
-        auto y = (std::numeric_limits<long double>::max)();
-        memcpy(buf,&x,sizeof(long double));
-        for (size_t i = sizeof(buf) -1; i+1 > 0; --i)
-        {
-            std::cout << std::hex << (int)buf[i] << " ";
-        }
-        std::cout << "\n";
-        auto val1 = jsoncons::detail::little_to_native<long double>(buf, buf + sizeof(long double), &endp);
-        std::cout << "float 128 val1: " << val1 << "\n";
-
-        memcpy(buf,&y,sizeof(long double));
-        for (size_t i = sizeof(buf) -1; i+1 > 0; --i)
-        {
-            std::cout << std::hex << (int)buf[i] << " ";
-        }
-        auto val2 = jsoncons::detail::little_to_native<long double>(buf, buf + sizeof(long double), &endp);
-        std::cout << "float 128 val2: " << val2 << "\n";
-
-        long double w = -1;
-        std::cout << "\n\n";
-        memcpy(buf,&w,sizeof(long double));
-        for (size_t i = sizeof(buf)-1; i+1 > 0; --i)
-        {
-            std::cout << std::hex << (int)buf[i] << " ";
-        }
-        std::cout << "\n\n";
-        auto val3 = jsoncons::detail::little_to_native<long double>(buf, buf + sizeof(long double), &endp);
-        std::cout << "float 128 val3: " << val3 << "\n";
-        long double z = 1;
-        memcpy(buf,&z,sizeof(long double));
-        for (size_t i = sizeof(buf) -1; i+1 > 0; --i)
-        {
-            std::cout << std::hex << (int)buf[i] << " ";
-        }
-        std::cout << "\n\n";
-
-        auto val4 = jsoncons::detail::little_to_native<long double>(buf, buf + sizeof(long double), &endp);
-        std::cout << "float 128 val4: " << val4 << "\n";
-
         const std::vector<uint8_t> input = {
             0xD8, // Tag
                 0x53, // Tag 83, float128, little endian, Typed Array
             0x58,0x40, // Byte string value of length 64
-                0x00,0x00,0x00,0x00,0x00,0x00,0xff,0xfe,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,
-                0x00,0x00,0x7f,0xfe,0x56,0xbf,0xbf,0xff,0x80,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
-                0x00,0x00,0x7f,0xfe,0x56,0xbf,0x3f,0xff,0x80,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
-                0x00,0x00,0x00,0x00,0x00,0x00,0x7f,0xfe,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff 
+                0xff,0xfe,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,
+                0x7f,0xfe,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,
+                0xbf,0xff,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+                0x3f,0xff,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
         };
 
-        json j = cbor::decode_cbor<json>(input);
-        REQUIRE(j.is_array());
+        //json j = cbor::decode_cbor<json>(input);
+        //REQUIRE(j.is_array());
         //REQUIRE(j.size() == 2);
 
-        std::cout << "Tag 83\n" << pretty_print(j) << "\n";
         //CHECK(j[0].as<double>() == std::numeric_limits<double>::lowest());
         //CHECK(j[1].as<double>() == (std::numeric_limits<double>::max)());
     }
@@ -473,68 +424,19 @@ TEST_CASE("cbor tagged array tests")
 
     SECTION("Tag 87, float128, little endian")
     {
-        std::cout << "sizeof(long double): " << sizeof(long double) << "\n";
-
-        const uint8_t* endp = nullptr;
-
-        uint8_t buf[sizeof(long double)];
-        auto x = std::numeric_limits<long double>::lowest();
-        auto y = (std::numeric_limits<long double>::max)();
-        memcpy(buf,&x,sizeof(long double));
-        for (auto b : buf)
-        {
-            std::cout << std::hex << (int)b << " ";
-        }
-        std::cout << "\n";
-        auto val1 = jsoncons::detail::little_to_native<long double>(buf, buf + sizeof(long double), &endp);
-        std::cout << "float 128 val1: " << val1 << "\n";
-
-        memcpy(buf,&y,sizeof(long double));
-        for (auto b : buf)
-        {
-            std::cout << std::hex << (int)b << " ";
-        }
-        auto val2 = jsoncons::detail::little_to_native<long double>(buf, buf + sizeof(long double), &endp);
-        std::cout << "float 128 val2: " << val2 << "\n";
-
-        long double w = -1;
-        std::cout << "\n\n";
-        memcpy(buf,&w,sizeof(long double));
-        for (auto b : buf)
-        {
-            std::cout << std::hex << (int)b << " ";
-        }
-        auto val3 = jsoncons::detail::little_to_native<long double>(buf, buf + sizeof(long double), &endp);
-        std::cout << "float 128 val3: " << val3 << "\n";
-        std::cout << "\n\n";
-        long double z = 1;
-        memcpy(buf,&z,sizeof(long double));
-        for (auto b : buf)
-        {
-            std::cout << std::hex << (int)b << " ";
-        }
-        std::cout << "\n\n";
-
-        auto val4 = jsoncons::detail::little_to_native<long double>(buf, buf + sizeof(long double), &endp);
-        std::cout << "float 128 val4: " << val4 << "\n";
-
         const std::vector<uint8_t> input = {
             0xD8, // Tag
                 0x57, // Tag 87, float128, little endian, Typed Array
             0x58,0x40, // Byte string value of length 64
-                0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xfe,0xff,0x00,0x00,0x00,0x00,0x00,0x00,
-                0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x80,0xff,0xbf,0xbf,0x56,0xfe,0x7f,0x00,0x00,
-                0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x80,0xff,0x3f,0xbf,0x56,0xfe,0x7f,0x00,0x00,
-                0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xfe,0x7f,0x00,0x00,0x00,0x00,0x00,0x00
+                0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xfe,0xff,
+                0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xfe,0x7f,
+                0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0xff,0xbf,
+                0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0xff,0x3f 
         };
 
-        json j = cbor::decode_cbor<json>(input);
-        REQUIRE(j.is_array());
+        //json j = cbor::decode_cbor<json>(input);
+        //REQUIRE(j.is_array());
         //REQUIRE(j.size() == 2);
-
-        std::cout << "Tag 87\n" << pretty_print(j) << "\n";
-        //CHECK(j[0].as<double>() == std::numeric_limits<double>::lowest());
-        //CHECK(j[1].as<double>() == (std::numeric_limits<double>::max)());
     }
 
 } 
