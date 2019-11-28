c58a4b62de6729a4ae4d560ed355cff2f0ea11cb
Daniel Parker <danielaparker@yahoo.com>
2019-11-27 21:43:03 -0500

    Version update

--- include/jsoncons/config/version.hpp
+++ include/jsoncons/config/version.hpp
@@ -10,7 +10,7 @@
 #include <iostream>
     
 #define JSONCONS_VERSION_MAJOR 0
-#define JSONCONS_VERSION_MINOR 139
+#define JSONCONS_VERSION_MINOR 140
 #define JSONCONS_VERSION_PATCH 0
 
 namespace jsoncons {
