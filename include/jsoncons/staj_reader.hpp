e06720398a10d84a956ddd86b7a0112f8c32d3dd
Daniel Parker <danielaparker@yahoo.com>
2019-12-25 23:30:46 -0500

    Improve  typed array tag types

--- include/jsoncons/staj_reader.hpp
+++ include/jsoncons/staj_reader.hpp
@@ -501,29 +501,29 @@ enum class staj_reader_state
     shape
 };
 
-struct uint8_array_arg_t {};
+struct uint8_array_arg_t {explicit uint8_array_arg_t() = default; };
 constexpr uint8_array_arg_t uint8_array_arg = uint8_array_arg_t();
-struct uint16_array_arg_t {};
-struct uint32_array_arg_t {};
+struct uint16_array_arg_t {explicit uint16_array_arg_t() = default; };
+struct uint32_array_arg_t {explicit uint32_array_arg_t() = default; };
 constexpr uint32_array_arg_t uint32_array_arg = uint32_array_arg_t();
-struct uint64_array_arg_t {};
+struct uint64_array_arg_t {explicit uint64_array_arg_t() = default; };
 constexpr uint64_array_arg_t uint64_array_arg = uint64_array_arg_t();
-struct int8_array_arg_t {};
+struct int8_array_arg_t {explicit int8_array_arg_t() = default; };
 constexpr int8_array_arg_t int8_array_arg = int8_array_arg_t();
-struct int16_array_arg_t {};
+struct int16_array_arg_t {explicit int16_array_arg_t() = default; };
 constexpr int16_array_arg_t int16_array_arg = int16_array_arg_t();
-struct int32_array_arg_t {};
+struct int32_array_arg_t {explicit int32_array_arg_t() = default; };
 constexpr int32_array_arg_t int32_array_arg = int32_array_arg_t();
-struct int64_array_arg_t {};
+struct int64_array_arg_t {explicit int64_array_arg_t() = default; };
 constexpr int64_array_arg_t int64_array_arg = int64_array_arg_t();
 constexpr uint16_array_arg_t uint16_array_arg = uint16_array_arg_t();
-struct half_array_arg_t {};
+struct half_array_arg_t {explicit half_array_arg_t() = default; };
 constexpr half_array_arg_t half_array_arg = half_array_arg_t();
-struct float_array_arg_t {};
+struct float_array_arg_t {explicit float_array_arg_t() = default; };
 constexpr float_array_arg_t float_array_arg = float_array_arg_t();
-struct double_array_arg_t {};
+struct double_array_arg_t {explicit double_array_arg_t() = default; };
 constexpr double_array_arg_t double_array_arg = double_array_arg_t();
-struct float128_array_arg_t {};
+struct float128_array_arg_t {explicit float128_array_arg_t() = default; };
 constexpr float128_array_arg_t float128_array_arg = float128_array_arg_t();
 
 enum typed_array_type {uint8_value=1,uint16_value,uint32_value,uint64_value,
