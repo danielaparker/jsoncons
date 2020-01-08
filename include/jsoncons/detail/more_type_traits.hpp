2a6976ab2bb0d846f624b855f54f1b273461c23d
Daniel Parker <danielaparker@yahoo.com>
2020-01-07 22:47:19 -0500

    Workaround for vs2015 compiler bug 3

--- include/jsoncons/detail/more_type_traits.hpp
+++ include/jsoncons/detail/more_type_traits.hpp
@@ -190,6 +190,9 @@ struct is_constructible_from_const_pointer_and_size<T,
 
 // has_size_and_data
 
+
+// has_size_and_data
+#if !(defined(_MSC_VER) && _MSC_VER <= 1900)
 template<class T, class Enable=void>
 struct has_data_and_size : std::false_type{};
 
@@ -200,6 +203,11 @@ struct has_data_and_size
     typename std::enable_if<!std::is_void<decltype(std::declval<C>().size())>::value &&
                             !std::is_void<decltype(std::declval<C>().data())>::value>::type
 > : std::true_type{};
+#else
+template<class T, class Enable=void>
+struct has_data_and_size : std::true_type{};
+
+#endif
 
 // has_reserve
 
