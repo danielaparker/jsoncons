f0970d83e5e0483a2c938a4ce4736eae7c2bd19d
Daniel Parker <danielaparker@yahoo.com>
2020-01-10 11:22:39 -0500

    jmespath - slice issue

--- include/jsoncons/detail/optional.hpp
+++ include/jsoncons/detail/optional.hpp
@@ -61,16 +61,18 @@ namespace detail
         /*constexpr */optional(U&& value,
                            typename std::enable_if<std::is_constructible<T, U&&>::value &&
                            std::is_convertible<T, U&&>::value>::type * = 0) // (8)
-            : valuep_(::new(&storage_)value_type(std::forward<U>(value)))
+            : valuep_(nullptr)
         {
+            valuep_ = ::new(&storage_)value_type(std::forward<U>(value));
         }
 
         template <class U = T>
         explicit /*constexpr*/ optional(U&& value,
                                     typename std::enable_if<std::is_constructible<T, U&&>::value &&
                                                             !std::is_convertible<T, U&&>::value>::type* = 0) // (8)
-            : valuep_(::new(&storage_)value_type(std::forward<U>(value)))
+            : valuep_(nullptr)
         {
+            valuep_ = ::new(&storage_)value_type(std::forward<U>(value));
         }
         ~optional()
         {
