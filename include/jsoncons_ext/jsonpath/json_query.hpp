ffb369fb1ff295473359766fc21390421d644613
Daniel Parker <danielaparker@yahoo.com>
2020-02-03 12:59:36 -0500

    jsonpath cleanup

--- include/jsoncons_ext/jsonpath/json_query.hpp
+++ include/jsoncons_ext/jsonpath/json_query.hpp
@@ -1,4 +1,4 @@
-// Copyright 2013 Daniel Parkerpath_single_quoted
+// Copyright 2013 Daniel Parker
 // Distributed under the Boost license, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
