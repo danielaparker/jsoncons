68c8018d4808e5030e6be0fedc9d82b9af1fccb6
Daniel Parker <danielaparker@yahoo.com>
2020-02-13 15:36:38 -0500

    Work on jmespath

--- include/jsoncons_ext/jsonpatch/jsonpatch_error.hpp
+++ include/jsoncons_ext/jsonpatch/jsonpatch_error.hpp
@@ -12,66 +12,97 @@
 
 namespace jsoncons { namespace jsonpatch {
 
-    enum class jsonpatch_errc 
+class jsonpatch_error : public std::system_error, public virtual json_exception
+{
+public:
+    jsonpatch_error(const std::error_code& ec)
+        : std::system_error(ec)
     {
-        success = 0,
-        invalid_patch = 1,
-        test_failed,
-        add_failed,
-        remove_failed,
-        replace_failed,
-        move_failed,
-        copy_failed
-
-    };
-
-    class jsonpatch_error_category_impl
-       : public std::error_category
-    {
-    public:
-        const char* name() const noexcept override
-        {
-            return "jsoncons/jsonpatch";
-        }
-        std::string message(int ev) const override
-        {
-            switch (static_cast<jsonpatch_errc>(ev))
-            {
-                case jsonpatch_errc::invalid_patch:
-                    return "Invalid JSON Patch document";
-                case jsonpatch_errc::test_failed:
-                    return "JSON Patch test operation failed";
-                case jsonpatch_errc::add_failed:
-                    return "JSON Patch add operation failed";
-                case jsonpatch_errc::remove_failed:
-                    return "JSON Patch remove operation failed";
-                case jsonpatch_errc::replace_failed:
-                    return "JSON Patch replace operation failed";
-                case jsonpatch_errc::move_failed:
-                    return "JSON Patch move operation failed";
-                case jsonpatch_errc::copy_failed:
-                    return "JSON Patch copy operation failed";
-                default:
-                    return "Unknown JSON Patch error";
-            }
-        }
-    };
-
-    inline
-    const std::error_category& jsonpatch_error_category()
-    {
-      static jsonpatch_error_category_impl instance;
-      return instance;
     }
 
-    inline 
-    std::error_code make_error_code(jsonpatch_errc result)
+    jsonpatch_error(const std::error_code& ec, const std::string& what_arg)
+        : std::system_error(ec, what_arg)
     {
-        return std::error_code(static_cast<int>(result),jsonpatch_error_category());
     }
 
-} // jsonpatch
-} // jsoncons
+    jsonpatch_error(const std::error_code& ec, const char* what_arg)
+        : std::system_error(ec, what_arg)
+    {
+    }
+
+    jsonpatch_error(const jsonpatch_error& other) = default;
+
+    jsonpatch_error(jsonpatch_error&& other) = default;
+
+    const char* what() const noexcept override
+    {
+        return std::system_error::what();
+    }
+
+    jsonpatch_error& operator=(const jsonpatch_error& e) = default;
+    jsonpatch_error& operator=(jsonpatch_error&& e) = default;
+private:
+};
+
+enum class jsonpatch_errc 
+{
+    success = 0,
+    invalid_patch = 1,
+    test_failed,
+    add_failed,
+    remove_failed,
+    replace_failed,
+    move_failed,
+    copy_failed
+
+};
+
+class jsonpatch_error_category_impl
+   : public std::error_category
+{
+public:
+    const char* name() const noexcept override
+    {
+        return "jsoncons/jsonpatch";
+    }
+    std::string message(int ev) const override
+    {
+        switch (static_cast<jsonpatch_errc>(ev))
+        {
+            case jsonpatch_errc::invalid_patch:
+                return "Invalid JSON Patch document";
+            case jsonpatch_errc::test_failed:
+                return "JSON Patch test operation failed";
+            case jsonpatch_errc::add_failed:
+                return "JSON Patch add operation failed";
+            case jsonpatch_errc::remove_failed:
+                return "JSON Patch remove operation failed";
+            case jsonpatch_errc::replace_failed:
+                return "JSON Patch replace operation failed";
+            case jsonpatch_errc::move_failed:
+                return "JSON Patch move operation failed";
+            case jsonpatch_errc::copy_failed:
+                return "JSON Patch copy operation failed";
+            default:
+                return "Unknown JSON Patch error";
+        }
+    }
+};
+
+inline
+const std::error_category& jsonpatch_error_category()
+{
+  static jsonpatch_error_category_impl instance;
+  return instance;
+}
+
+inline 
+std::error_code make_error_code(jsonpatch_errc result)
+{
+    return std::error_code(static_cast<int>(result),jsonpatch_error_category());
+}
+
+}}
 
 namespace std {
     template<>
@@ -79,31 +110,5 @@ namespace std {
     {
     };
 }
-
-namespace jsoncons { namespace jsonpatch {
-
-    class jsonpatch_error : public std::system_error, public virtual json_exception
-    {
-    public:
-        jsonpatch_error(const std::error_code& ec)
-            : std::system_error(ec)
-        {
-        }
-
-        jsonpatch_error(const jsonpatch_error& other) = default;
-
-        jsonpatch_error(jsonpatch_error&& other) = default;
-
-        const char* what() const noexcept override
-        {
-            return this->code().message().c_str();
-        }
-
-        jsonpatch_error& operator=(const jsonpatch_error& e) = default;
-        jsonpatch_error& operator=(jsonpatch_error&& e) = default;
-    private:
-    };
-} // jsonpatch
-} // jsoncons
 
 #endif
