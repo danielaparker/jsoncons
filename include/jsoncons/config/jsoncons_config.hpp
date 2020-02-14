b168bdc162d740bb99f275bd3fbb64cf276b76a3
Daniel Parker <danielaparker@yahoo.com>
2020-02-14 00:26:46 -0500

    Improve JSONCONS_STRING_LITERAL

--- include/jsoncons/config/jsoncons_config.hpp
+++ include/jsoncons/config/jsoncons_config.hpp
@@ -249,8 +249,8 @@ namespace detail {
 #define JSONCONS_STRING_LITERAL(name, ...) \
    template <class CharT> \
    const jsoncons::basic_string_view<CharT>& name() {\
-       constexpr CharT s[] = { __VA_ARGS__};\
-       constexpr jsoncons::basic_string_view<CharT> sv(s, sizeof(s) / sizeof(CharT));\
+       static constexpr CharT s[] = { __VA_ARGS__};\
+       static const jsoncons::basic_string_view<CharT> sv(s, sizeof(s) / sizeof(CharT));\
        return sv;\
    }
 
