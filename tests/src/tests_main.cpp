32ee41f89d681be7436e298e59ef81fb31c683d5
Daniel Parker <danielaparker@yahoo.com>
2020-02-26 21:24:35 -0500

    Improve jsoncons::optional

--- tests/src/tests_main.cpp
+++ tests/src/tests_main.cpp
@@ -1,2 +1,18 @@
 #define CATCH_CONFIG_MAIN
 #include <catch/catch.hpp>
+
+#include <jsoncons/config/jsoncons_config.hpp>
+
+
+TEST_CASE("This here test")
+{
+#if defined(JSONCONS_HAS_STD_OPTIONAL)
+std::cout << "Has std::optional" << "\n";
+std::optional<int64_t> end{};
+std::optional<int64_t> end2{};
+
+end = end2;
+#else
+std::cout << "Hasn't std::optional" << "\n";
+#endif
+}
