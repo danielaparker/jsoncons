6614df8a9fad1121db638e4bc7bb120bfc0ada25
danielaparker <danielaparker@yahoo.com>
2020-04-14 14:17:20 -0400

    Fix clusterfuzz test

--- tests/src/clusterfuzz/clusterfuzz_tests.cpp
+++ tests/src/clusterfuzz/clusterfuzz_tests.cpp
@@ -15,9 +15,11 @@ TEST_CASE("clusterfuzz issues")
     SECTION("issue 21589")
     {
         std::string pathname = "input/clusterfuzz/clusterfuzz-testcase-minimized-fuzz_parse-5763671533027328";
+        json_options options;
+        options.max_depth(std::numeric_limits<int>::max());
 
         std::ifstream is(pathname);
-        REQUIRE_THROWS_WITH(json::parse(is), Catch::Matchers::Contains(json_error_category_impl().message((int)json_errc::expected_comma_or_right_bracket).c_str()));
+        REQUIRE_THROWS_WITH(json::parse(is, options), Catch::Matchers::Contains(json_error_category_impl().message((int)json_errc::expected_comma_or_right_bracket).c_str()));
     }
 }
 
