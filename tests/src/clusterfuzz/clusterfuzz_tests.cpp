284e6d0737d490f9a0e09923a87c4df89a893410
danielaparker <danielaparker@yahoo.com>
2020-04-15 19:44:08 -0400

    test clusterfuzz issue 21709

--- tests/src/clusterfuzz/clusterfuzz_tests.cpp
+++ tests/src/clusterfuzz/clusterfuzz_tests.cpp
@@ -2,6 +2,7 @@
 // Distributed under Boost license
 
 #include <jsoncons/json.hpp>
+#include <jsoncons_ext/cbor/cbor.hpp>
 #include <vector>
 #include <utility>
 #include <ctime>
@@ -21,5 +22,15 @@ TEST_CASE("clusterfuzz issues")
         std::ifstream is(pathname);
         REQUIRE_THROWS_WITH(json::parse(is, options), Catch::Matchers::Contains(json_error_category_impl().message((int)json_errc::expected_comma_or_right_bracket).c_str()));
     }
+    SECTION("issue 21709")
+    {
+        std::string pathname = "input/clusterfuzz/clusterfuzz-testcase-minimized-fuzz_cbor-5740910806827008.fuzz";
+
+        std::ifstream is(pathname);
+        cbor::cbor_options options;
+        options.max_nesting_depth(std::numeric_limits<int>::max());
+        REQUIRE_THROWS_WITH(cbor::decode_cbor<json>(is, options), Catch::Matchers::Contains(cbor::cbor_error_category_impl().message((int)cbor::cbor_errc::invalid_utf8_text_string).c_str()));
+    }
+
 }
 
