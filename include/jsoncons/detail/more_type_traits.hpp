26219e786a32f2e5e23ea44e16d1bf3d42218cfa
danielaparker <danielaparker@yahoo.com>
2020-05-23 22:24:32 -0400

    Improve more_type_traits is_back_insertable

--- include/jsoncons/detail/more_type_traits.hpp
+++ include/jsoncons/detail/more_type_traits.hpp
@@ -271,7 +271,7 @@ namespace detail {
 
     template<class Container>
     using
-    container_push_back_t = decltype(std::declval<Container>().push_back(typename Container::value_type()));
+    container_push_back_t = decltype(std::declval<Container>().push_back(*(typename Container::pointer())));
 
     template<class Container>
     using
