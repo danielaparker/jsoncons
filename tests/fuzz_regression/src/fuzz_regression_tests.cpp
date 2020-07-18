796a8ab375b0baa256385448403ec0f7fdcb87f8
danielaparker <danielaparker@yahoo.com>
2020-07-18 18:15:31 -0400

    Upate CMakeLists.txt

--- tests/fuzz_regression/src/fuzz_regression_tests.cpp
+++ tests/fuzz_regression/src/fuzz_regression_tests.cpp
@@ -509,7 +509,7 @@ TEST_CASE("oss-fuzz issues")
         try {
            json j2 = ubjson::decode_ubjson<json>(is);
         }
-        catch(jsoncons::ser_error) 
+        catch(const jsoncons::ser_error&) 
         {
             //std::cout << e.what() << "\n\n";
         }
