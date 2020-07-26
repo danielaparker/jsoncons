b648b75140139c194ba636b27471a1657938ec18
danielaparker <danielaparker@yahoo.com>
2020-07-26 16:00:41 -0400

    Removed json_const_pointer_arg

--- include/jsoncons/json_type.hpp
+++ include/jsoncons/json_type.hpp
@@ -36,8 +36,7 @@ namespace jsoncons {
         byte_string_value = 0x08,
         array_value = 0x09,
         empty_object_value = 0x0a,
-        object_value = 0x0b,
-        json_const_pointer = 0x0c
+        object_value = 0x0b
     };
 
 } // jsoncons
