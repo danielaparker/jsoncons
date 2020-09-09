86504c720327cf913eb3bb82214561b2c9d175e2
danielaparker <danielaparker@yahoo.com>
2020-09-09 15:33:12 -0400

    Create yaml folder

--- tests/yaml/src/yaml_parser_tests.cpp
+++ tests/yaml/src/yaml_parser_tests.cpp
@@ -13,202 +13,35 @@
 
 using namespace jsoncons;
 
-TEST_CASE("test_parse_empty_object")
+TEST_CASE("yaml test")
 {
-    jsoncons::json_decoder<json> decoder;
-    yaml::yaml_parser parser;
+    const std::string input = R"(
+invoice: 34843
+date   : !!str 2001-01-23
+bill-to: &id001
+    given  : Chris
+    family : Dumars
+    address:
+        lines: |
+            458 Walkman Dr.
+            Suite #292
+)";
 
-    parser.reset();
 
-    static std::string s("{}");
+    SECTION("test1")
+    {
+        jsoncons::json_decoder<json> decoder;
+        yaml::yaml_parser parser;
 
-    parser.update(s.data(),s.length());
-    parser.parse_some(decoder);
-    parser.finish_parse(decoder);
-    CHECK(parser.done());
+        parser.reset();
 
-    json j = decoder.get_result();
+        parser.update(input.data(),input.length());
+        parser.parse_some(decoder);
+        parser.finish_parse(decoder);
+        CHECK(parser.done());
+
+        json j = decoder.get_result();
+        std::cout << pretty_print(j) << "\n\n";
+    }
 }
-
-TEST_CASE("test_parse_array")
-{
-    jsoncons::json_decoder<json> decoder;
-    yaml::yaml_parser parser;
-
-    parser.reset();
-
-    static std::string s("[]");
-
-    parser.update(s.data(),s.length());
-    parser.parse_some(decoder);
-    parser.finish_parse(decoder);
-    CHECK(parser.done());
-
-    json j = decoder.get_result();
-}
-
-TEST_CASE("test_parse_string")
-{
-    jsoncons::json_decoder<json> decoder;
-    yaml::yaml_parser parser;
-
-    parser.reset();
-
-    static std::string s("\"\"");
-
-    parser.update(s.data(),s.length());
-    parser.parse_some(decoder);
-    parser.finish_parse(decoder);
-    CHECK(parser.done());
-
-    json j = decoder.get_result();
-}
-
-TEST_CASE("test_parse_integer")
-{
-    jsoncons::json_decoder<json> decoder;
-    yaml::yaml_parser parser;
-
-    parser.reset();
-
-    static std::string s("10");
-
-    parser.update(s.data(),s.length());
-    parser.parse_some(decoder);
-    parser.finish_parse(decoder);
-    CHECK(parser.done());
-
-    json j = decoder.get_result();
-}
-
-TEST_CASE("test_parse_integer_space")
-{
-    jsoncons::json_decoder<json> decoder;
-    yaml::yaml_parser parser;
-
-    parser.reset();
-
-    static std::string s("10 ");
-
-    parser.update(s.data(),s.length());
-    parser.parse_some(decoder);
-    parser.finish_parse(decoder);
-    CHECK(parser.done());
-
-    json j = decoder.get_result();
-}
-
-TEST_CASE("test_parse_double_space")
-{
-    jsoncons::json_decoder<json> decoder;
-    yaml::yaml_parser parser;
-
-    parser.reset();
-
-    static std::string s("10.0 ");
-
-    parser.update(s.data(),s.length());
-    parser.parse_some(decoder);
-    parser.finish_parse(decoder);
-    CHECK(parser.done());
-
-    json j = decoder.get_result();
-}
-
-TEST_CASE("test_parse_false")
-{
-    jsoncons::json_decoder<json> decoder;
-    yaml::yaml_parser parser;
-
-    parser.reset();
-
-    static std::string s("false");
-
-    parser.update(s.data(),s.length());
-    parser.parse_some(decoder);
-    parser.finish_parse(decoder);
-    CHECK(parser.done());
-
-    json j = decoder.get_result();
-}
-
-TEST_CASE("test_parse_true")
-{
-    jsoncons::json_decoder<json> decoder;
-    yaml::yaml_parser parser;
-
-    parser.reset();
-
-    static std::string s("true");
-
-    parser.update(s.data(),s.length());
-    parser.parse_some(decoder);
-    parser.finish_parse(decoder);
-    CHECK(parser.done());
-
-    json j = decoder.get_result();
-}
-
-TEST_CASE("test_parse_null")
-{
-    jsoncons::json_decoder<json> decoder;
-    yaml::yaml_parser parser;
-
-    parser.reset();
-
-    static std::string s("null");
-
-    parser.update(s.data(),s.length());
-    parser.parse_some(decoder);
-    parser.finish_parse(decoder);
-    CHECK(parser.done());
-
-    json j = decoder.get_result();
-}
-
-TEST_CASE("test_parse_array_string")
-{
-    jsoncons::json_decoder<json> decoder;
-    yaml::yaml_parser parser;
-
-    parser.reset();
-
-    static std::string s1("[\"\"");
-
-    parser.update(s1.data(),s1.length());
-    parser.parse_some(decoder);
-    CHECK_FALSE(parser.done());
-    static std::string s2("]");
-    parser.update(s2.data(), s2.length());
-    parser.parse_some(decoder);
-    parser.finish_parse(decoder);
-    CHECK(parser.done());
-
-    json j = decoder.get_result();
-}
-
-TEST_CASE("test_incremental_parsing")
-{
-    jsoncons::json_decoder<json> decoder;
-    yaml::yaml_parser parser;
-
-    parser.reset();
-
-    parser.update("[fal",4);
-    parser.parse_some(decoder);
-    CHECK_FALSE(parser.done());
-    CHECK(parser.source_exhausted());
-    parser.update("se]",3);
-    parser.parse_some(decoder);
-
-    parser.finish_parse(decoder);
-    CHECK(parser.done());
-
-    json j = decoder.get_result();
-    REQUIRE(j.is_array());
-    CHECK_FALSE(j[0].as<bool>());
-}
-
-
-
 
