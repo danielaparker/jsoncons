a0f871cbea6ee187bc28fe61512ec14516fda0b5
danielaparker <danielaparker@yahoo.com>
2020-12-15 17:14:34 -0500

    jsonpath_new

--- tests/jsonpath_new/src/jsonpath_test_suite.cpp
+++ tests/jsonpath_new/src/jsonpath_test_suite.cpp
@@ -107,9 +107,9 @@ TEST_CASE("jsonpath-tests")
         //jsonpath_tests("./jsonpath_new/input/compliance/slice.json");
         //jsonpath_tests("./jsonpath_new/input/compliance/jsonpath-tests.json");
         //jsonpath_tests("./jsonpath_new/input/compliance/identifiers.json");
-        //jsonpath_tests("./jsonpath_new/input/compliance/recursive-descent.json"); 
+        jsonpath_tests("./jsonpath_new/input/compliance/recursive-descent.json"); 
         //jsonpath_tests("./jsonpath_new/input/compliance/indices.json");
-        jsonpath_tests("./jsonpath_new/input/compliance/wildcard.json");
+        //jsonpath_tests("./jsonpath_new/input/compliance/wildcard.json");
     }
 }
 
