876826f0a482a0b7118c298ba93d91a5b977f6f4
danielaparker <danielaparker@yahoo.com>
2021-02-03 20:53:13 -0500

    jsonpath doc update

--- include/jsoncons_ext/jsonpath/json_query.hpp
+++ include/jsoncons_ext/jsonpath/json_query.hpp
@@ -1431,35 +1431,25 @@ namespace jsoncons { namespace jsonpath {
                     case path_state::unquoted_string: 
                         switch (*p_)
                         {
-                            case '(':
-                            case ')':
-                            case ']':
-                            case '[':
-                            case '.':
-                            case ',':
-                            case ' ':case '\t':
-                            case '\r':
-                            case '\n':
-                            case '!':
-                            case '=':
-                            case '<':
-                            case '>':
-                            case '~':
-                            case '|':
-                            case '&':
-                            case '+':
-                            case '-':
-                            case '*':
-                            case '/':
-                            case '@':
-                            case '$':
-                            case '?':
-                                state_stack_.pop_back(); // unquoted_string
-                                break;
-                            default:
+                            case 'a':case 'b':case 'c':case 'd':case 'e':case 'f':case 'g':case 'h':case 'i':case 'j':case 'k':case 'l':case 'm':case 'n':case 'o':case 'p':case 'q':case 'r':case 's':case 't':case 'u':case 'v':case 'w':case 'x':case 'y':case 'z':
+                            case 'A':case 'B':case 'C':case 'D':case 'E':case 'F':case 'G':case 'H':case 'I':case 'J':case 'K':case 'L':case 'M':case 'N':case 'O':case 'P':case 'Q':case 'R':case 'S':case 'T':case 'U':case 'V':case 'W':case 'X':case 'Y':case 'Z':
+                            case '0':case '1':case '2':case '3':case '4':case '5':case '6':case '7':case '8':case '9':
+                            case '_':
                                 buffer.push_back(*p_);
                                 ++p_;
                                 ++column_;
+                                break;
+                            default:
+                                if (typename std::make_unsigned<char_type>::type(*p_) > 127)
+                                {
+                                    buffer.push_back(*p_);
+                                    ++p_;
+                                    ++column_;
+                                }
+                                else
+                                {
+                                    state_stack_.pop_back(); // unquoted_string
+                                }
                                 break;
                         };
                         break;                    
