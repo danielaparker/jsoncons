7bfe26d02fb2358afc3b567b2dacd0836185571b
cschreib-ibex <c.schreiber@ibexinnovations.co.uk>
2022-12-01 17:02:10 +0000

    Add && JSONCONS_HAS_STD_FROM_CHARS to allow disabling

--- include/jsoncons/config/compiler_support.hpp
+++ include/jsoncons/config/compiler_support.hpp
@@ -130,7 +130,7 @@
 #   endif // (_MSC_VER >= 1924 && MSVC_LANG >= 201703)
 #  endif // defined(_MSC_VER)
 #endif
-#if defined(JSONCONS_HAS_STD_FROM_CHARS)
+#if defined(JSONCONS_HAS_STD_FROM_CHARS) && JSONCONS_HAS_STD_FROM_CHARS
 #include <charconv>
 #endif
 
