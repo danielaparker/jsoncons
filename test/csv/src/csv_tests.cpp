ec8e2af69abb14cd6f8fe83ca7f138352f320106
cschreib-ibex <c.schreiber@ibexinnovations.co.uk>
2022-12-02 09:09:15 +0000

    Remove conditional for csv_reader contructors test

--- test/csv/src/csv_tests.cpp
+++ test/csv/src/csv_tests.cpp
@@ -1497,7 +1497,6 @@ TEST_CASE("csv detect bom")
     }
 }
 
-#if !(defined(__GNUC__) || (defined(_MSC_VER) && _MSC_VER == 1900))
 TEST_CASE("csv_reader constructors")
 {
     const std::string input = R"(Date,1Y,2Y,3Y,5Y
@@ -1526,7 +1525,6 @@ TEST_CASE("csv_reader constructors")
         //std::cout << pretty_print(j) << "\n";
     }
 }
-#endif
 
 TEST_CASE("infinite loop")
 {
