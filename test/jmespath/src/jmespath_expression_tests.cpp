4e7fe66195ffd71a9a0eea85a3aa96b9118acfe0
Daniel Parker <danielaparker@yahoo.com>
2023-12-08 18:51:14 -0500

    gcc issue

--- test/jmespath/src/jmespath_expression_tests.cpp
+++ test/jmespath/src/jmespath_expression_tests.cpp
@@ -10,7 +10,7 @@
 #include <iostream>
 
 using jsoncons::json;
-//using jsoncons::ojson;
+using jsoncons::ojson;
 namespace jmespath = jsoncons::jmespath;
 
 TEST_CASE("jmespath_expression tests")
@@ -96,7 +96,7 @@ TEST_CASE("jmespath issue")
     }
     )";
 
-    auto doc = json::parse(jtext);
+    auto doc = ojson::parse(jtext);
 
     auto result = jmespath::search(doc, expr);
 
