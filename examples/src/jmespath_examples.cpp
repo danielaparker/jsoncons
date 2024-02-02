62be30665795defa35b771bc0fc31bcc284d87df
Daniel Parker <danielaparker@yahoo.com>
2024-02-01 21:31:45 -0500

    Example

--- examples/src/jmespath_examples.cpp
+++ examples/src/jmespath_examples.cpp
@@ -66,11 +66,53 @@ void jmespath_expression_example()
     std::cout << pretty_print(result) << "\n\n";
 }
 
+#if defined(_MSC_VER)
+
+#include <execution>
+#include <concurrent_vector.h> // microsoft PPL library
+
+void query_json_lines_in_parallel() 
+{
+
+    std::vector<std::string> lines = { {
+        R"({"name": "Seattle", "state" : "WA"})",
+        R"({ "name": "New York", "state" : "NY" })",
+        R"({ "name": "Bellevue", "state" : "WA" })",
+        R"({ "name": "Olympia", "state" : "WA" })"
+} };
+
+    auto expr = jsoncons::jmespath::make_expression<jsoncons::json>(
+        R"([@][?state=='WA'].name)");
+
+    concurrency::concurrent_vector<std::string> result;
+
+    auto f = [&](const std::string& line)
+        {
+            const auto j = jsoncons::json::parse(line);
+            const auto r = expr.evaluate(j);
+            if (!r.empty())
+                result.push_back(r.at(0).as<std::string>());
+        };
+
+    std::for_each(std::execution::par, lines.begin(), lines.end(), f);
+
+    for (const auto& s : result)
+    {
+        std::cout << s << "\n";
+    }
+}
+
+#endif
+
 int main()
 {
     std::cout << "\nJMESPath examples\n\n";
     search_example();
     jmespath_expression_example();
+
+#if defined(_MSC_VER)
+    query_json_lines_in_parallel();
+#endif
 
     std::cout << "\n";
 }
