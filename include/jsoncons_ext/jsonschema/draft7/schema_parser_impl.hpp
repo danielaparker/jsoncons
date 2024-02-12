1c9c675be310c1ece6ba16bccacdd24e606feaf4
Daniel Parker <danielaparker@yahoo.com>
2024-02-12 15:13:00 -0500

    make_object_schema_validator

--- include/jsoncons_ext/jsonschema/draft7/schema_parser_impl.hpp
+++ include/jsoncons_ext/jsonschema/draft7/schema_parser_impl.hpp
@@ -1191,6 +1191,48 @@ namespace draft7 {
                 std::move(additional_properties));
         }
                 
+
+        std::unique_ptr<dependencies_validator<Json>> make_dependencies_validator(const evaluation_context& eval_context, const compilation_context& context, 
+            const Json& sch)
+        {
+            uri schema_path = context.get_absolute_uri();
+            std::map<std::string, keyword_validator_type> dependent_required;
+            std::map<std::string, schema_validator_type> dependent_schemas;
+
+            auto it = sch.find("dependencies");
+            if (it != sch.object_range().end()) 
+            {
+                for (const auto& dep : it->value().object_range())
+                {
+                    switch (dep.value().type()) 
+                    {
+                        case json_type::array_value:
+                        {
+                            auto location = context.make_schema_path_with("dependencies");
+                            dependent_required.emplace(dep.key(), 
+                                make_required_validator(evaluation_context{eval_context, "dependencies"}, compilation_context(nullptr, std::vector<schema_location>{{location}}),
+                                    dep.value().template as<std::vector<std::string>>()));
+                            break;
+                        }
+                        case json_type::object_value:
+                        {
+                            std::string sub_keys[] = {"dependencies"};
+                            dependent_schemas.emplace(dep.key(),
+                                make_schema_validator(evaluation_context{eval_context, "dependencies"}, context, dep.value(), sub_keys));
+                            break;
+                        }
+                        default:
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return jsoncons::make_unique<dependencies_validator<Json>>(eval_context.eval_path(), std::move(schema_path),
+                std::move(dependent_required), std::move(dependent_schemas));
+        }
+
         void parse(const Json& sch) override
         {
             parse(sch, "#");
