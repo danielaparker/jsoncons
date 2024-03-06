946bb0b9b6bad8a7ff6a5892e2eaa38f5ce1fd33
Daniel Parker <danielaparker@yahoo.com>
2024-03-06 10:33:53 -0500

    Add $recursiveRef test

--- test/jsonschema/src/dynamic_ref_tests.cpp
+++ test/jsonschema/src/dynamic_ref_tests.cpp
@@ -17,13 +17,13 @@ using jsoncons::json;
 namespace jsonschema = jsoncons::jsonschema;
 namespace jsonpatch = jsoncons::jsonpatch;
 
-TEST_CASE("jsonschema $dynamicRef tests")
+TEST_CASE("jsonschema $recursiveRef tests")
 {
     std::string tree_schema_string = R"(
 {
-    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "$schema": "https://json-schema.org/draft/2019-09/schema",
     "$id": "https://example.com/tree",
-    "$dynamicAnchor": "node",
+    "$recursiveAnchor": true,
 
     "type": "object",
     "properties": {
@@ -31,7 +31,7 @@ TEST_CASE("jsonschema $dynamicRef tests")
         "children": {
             "type": "array",
             "items": {
-                "$dynamicRef": "#node"
+                "$recursiveRef": "#"
             }
         }
     }
@@ -40,9 +40,9 @@ TEST_CASE("jsonschema $dynamicRef tests")
 
     std::string strict_tree_schema_string = R"(
 {
-    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "$schema": "https://json-schema.org/draft/2019-09/schema",
     "$id": "https://example.com/strict-tree",
-    "$dynamicAnchor": "node",
+    "$recursiveAnchor": true,
 
     "$ref": "tree",
     "unevaluatedProperties": false
@@ -55,7 +55,7 @@ TEST_CASE("jsonschema $dynamicRef tests")
     
     auto resolver = [tree_schema](const jsoncons::uri& uri)
         {
-            std::cout << "resolver: " << uri.string() << "\n";
+            //std::cout << "resolver: " << uri.string() << "\n";
             if (uri.string() == "https://example.com/tree")
             {
                 return tree_schema;
@@ -104,6 +104,94 @@ TEST_CASE("jsonschema $dynamicRef tests")
         }
 
     }
+}
 
+TEST_CASE("jsonschema $dynamicRef tests")
+{
+    std::string tree_schema_string = R"(
+{
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "$id": "https://example.com/tree",
+    "$dynamicAnchor": "node",
+
+    "type": "object",
+    "properties": {
+        "data": true,
+        "children": {
+            "type": "array",
+            "items": {
+                "$dynamicRef": "#node"
+            }
+        }
+    }
+}
+    )";
+
+    std::string strict_tree_schema_string = R"(
+{
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "$id": "https://example.com/strict-tree",
+    "$dynamicAnchor": "node",
+
+    "$ref": "tree",
+    "unevaluatedProperties": false
+}
+    )";
+
+    json tree_schema = json::parse(tree_schema_string);
+
+    json strict_tree_schema = json::parse(strict_tree_schema_string);
+    
+    auto resolver = [tree_schema](const jsoncons::uri& uri)
+        {
+            //std::cout << "resolver: " << uri.string() << "\n";
+            if (uri.string() == "https://example.com/tree")
+            {
+                return tree_schema;
+            }
+            else
+            {
+                throw jsonschema::schema_error("Could not resolve " + uri.string() + "\n");
+            }
+        };
+    auto sch = jsonschema::make_schema(strict_tree_schema, resolver); 
+
+    SECTION("instance with misspelled field")
+    {
+        std::string data_string = R"(
+{
+    "children": [ { "daat": 1 } ]
+}
+        )";
+
+        try
+        {
+            // will throw schema_error if JSON Schema loading fails 
+
+            // Data
+            json data = json::parse(data_string);
+
+            jsonschema::json_validator<json> validator(sch); 
+
+            std::size_t error_count = 0;
+            auto reporter = [&](const jsonschema::validation_output& /*o*/)
+            {
+                //std::cout << "  Failed: " << "eval_path: " << o.eval_path().to_string() << ", schema_path: " << o.schema_path().string() << ", " << o.instance_location() << ": " << o.message() << "\n";
+                //for (const auto& err : o.nested_errors())
+                //{
+                    //std::cout << "  Nested error: " << err.instance_location() << ": " << err.message() << "\n";
+                //}
+                ++error_count;
+            };
+            validator.validate(data, reporter);
+            CHECK(error_count > 0);
+            //std::cout << "error_count: " << error_count << "\n";
+        }
+        catch (const std::exception& e)
+        {
+            std::cout << e.what() << "\n";
+        }
+
+    }
 }
 
