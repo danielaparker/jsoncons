48a15e42f070215f48c6a657117e7365f719fef8
Daniel Parker <danielaparker@yahoo.com>
2024-03-26 23:23:43 -0400

    Make send_json_event member

--- include/jsoncons_ext/csv/csv_cursor.hpp
+++ include/jsoncons_ext/csv/csv_cursor.hpp
@@ -218,7 +218,7 @@ public:
     void read_to(basic_json_visitor<CharT>& visitor,
                 std::error_code& ec) override
     {
-        if (send_json_event(cursor_visitor_.event(), visitor, *this, ec))
+        if (cursor_visitor_.event().send_json_event(visitor, *this, ec))
         {
             read_next(visitor, ec);
         }
