1d380cc5f2f9c209186419003504f64ea930ef4f
Daniel Parker <danielaparker@yahoo.com>
2024-04-27 15:45:18 -0400

    walk

--- test/jsonschema/src/json_schema_walk_tests.cpp
+++ test/jsonschema/src/json_schema_walk_tests.cpp
@@ -16,7 +16,7 @@
 using jsoncons::json;
 using jsoncons::ojson;
 namespace jsonschema = jsoncons::jsonschema;
-
+//#if 0
 TEST_CASE("jsonschema walk tests")
 {
     std::string schema_string = R"(
@@ -62,7 +62,7 @@ TEST_CASE("jsonschema walk tests")
     )";
 
     ojson schema = ojson::parse(schema_string);
-    jsonschema::json_schema<ojson> compiled = jsonschema::make_json_schema(schema); 
+    jsonschema::json_schema<ojson> compiled = jsonschema::make_json_schema(std::move(schema)); 
 
     SECTION("walk")
     {
@@ -86,10 +86,100 @@ TEST_CASE("jsonschema walk tests")
 }
         )";
 
+        // Data
+        ojson data = ojson::parse(data_string);
+ 
+        ojson expected = ojson::parse(R"(      
+{
+    "/fruits/0": "string",
+    "/fruits/1": "string",
+    "/fruits/2": "string",
+    "/fruits": "array",
+    "/vegetables/0/veggieName": "string",
+    "/vegetables/0/veggieLike": "boolean",
+    "/vegetables/0": "object",
+    "/vegetables/1/veggieName": "string",
+    "/vegetables/1/veggieLike": "boolean",
+    "/vegetables/1": "object",
+    "/vegetables": "array",
+    "": "object"
+}
+        )");
+
+        ojson result(jsoncons::json_object_arg);
+        auto reporter = [&](const std::string& keyword,
+            const ojson& schema, const jsoncons::uri& /*schema_location*/,
+            const ojson& /*instance*/, const jsoncons::jsonpointer::json_pointer& instance_location)
+        {
+            if (keyword == "type" && schema.is_object())
+            {
+                auto it = schema.find("type");
+                if (it != schema.object_range().end())
+                {
+                    result.try_emplace(instance_location.string(), it->value());
+                    //std::cout << instance_location.string() << ": " << it->value() << "\n";
+                }
+            }
+        };
+        compiled.walk(data, reporter);
+        CHECK(expected == result);
+        //std::cout << pretty_print(result) << "\n";
+    }
+} 
+//#endif
+#if 0
+TEST_CASE("jsonschema with $dynamicRef walk test")
+{
+    std::string schema_string = R"(
+{
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "$id": "https://test.json-schema.org/dynamic-ref-leaving-dynamic-scope/main",
+    "if": {
+        "$id": "first_scope",
+        "$defs": {
+            "thingy": {
+                "$comment": "this is first_scope#thingy",
+                "$dynamicAnchor": "thingy",
+                "type": "number"
+            }
+        }
+    },
+    "then": {
+        "$id": "second_scope",
+        "$ref": "start",
+        "$defs": {
+            "thingy": {
+                "$comment": "this is second_scope#thingy, the final destination of the $dynamicRef",
+                "$dynamicAnchor": "thingy",
+                "type": "null"
+            }
+        }
+    },
+    "$defs": {
+        "start": {
+            "$comment": "this is the landing spot from $ref",
+            "$id": "start",
+            "$dynamicRef": "inner_scope#thingy"
+        },
+        "thingy": {
+            "$comment": "this is the first stop for the $dynamicRef",
+            "$id": "inner_scope",
+            "$dynamicAnchor": "thingy",
+            "type": "string"
+        }
+    }
+}
+    )";
+
+    ojson schema = ojson::parse(schema_string);
+    jsonschema::json_schema<ojson> compiled = jsonschema::make_json_schema(std::move(schema)); 
+
+    SECTION("walk")
+    {
+        std::string data_string = R"(null)";
+
         try
         {
-            // will throw schema_error if JSON Schema compilation fails 
-
             // Data
             ojson data = ojson::parse(data_string);
 
@@ -97,6 +187,7 @@ TEST_CASE("jsonschema walk tests")
                 const ojson& schema, const jsoncons::uri& /*schema_location*/,
                 const ojson& /*instance*/, const jsoncons::jsonpointer::json_pointer& instance_location)
             {
+                std::cout << keyword << "\n";
                 if (keyword == "type" && schema.is_object())
                 {
                     auto it = schema.find("type");
@@ -112,7 +203,6 @@ TEST_CASE("jsonschema walk tests")
         {
             std::cout << e.what() << "\n";
         }
-
     }
 }
-
+#endif
