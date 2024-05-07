d1d3b376719861f981367fb279300385c9d33f92
Daniel Parker <danielaparker@yahoo.com>
2024-05-07 12:49:20 -0400

    optional value()

--- include/jsoncons/detail/optional.hpp
+++ include/jsoncons/detail/optional.hpp
@@ -250,16 +250,20 @@ namespace detail
 
         T& value() &
         {
-            return static_cast<bool>(*this)
-                       ? get()
-                       : JSONCONS_THROW(std::runtime_error("Bad optional access")), get();
+            if (has_value_)
+            {
+                return get();
+            }
+            JSONCONS_THROW(std::runtime_error("Bad optional access"));
         }
 
-        constexpr const T& value() const &
+        JSONCONS_CONSTEXPR const T& value() const &
         {
-            return static_cast<bool>(*this)
-                       ? get()
-                       : JSONCONS_THROW(std::runtime_error("Bad optional access")), get();
+            if (has_value_)
+            {
+                return get();
+            }
+            JSONCONS_THROW(std::runtime_error("Bad optional access"));
         }
 
         template <typename U>
