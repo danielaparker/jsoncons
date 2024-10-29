38f181d8515ca631bf479e7c59ed73dfa28161ac
Daniel Parker <danielaparker@yahoo.com>
2024-10-28 20:51:58 -0400

    Improve basic_json storage

--- include/jsoncons/basic_json.hpp
+++ include/jsoncons/basic_json.hpp
@@ -738,37 +738,9 @@ namespace jsoncons {
             semantic_tag tag_;
             pointer ptr_;
 
-            template <typename... Args>
-            void create(allocator_type alloc, Args&& ... args)
-            {
-                ptr_ = std::allocator_traits<allocator_type>::allocate(alloc, 1);
-                JSONCONS_TRY
-                {
-                    std::allocator_traits<allocator_type>::construct(alloc, extension_traits::to_plain_pointer(ptr_), 
-                        std::forward<Args>(args)...);
-                }
-                JSONCONS_CATCH(...)
-                {
-                    std::allocator_traits<allocator_type>::deallocate(alloc, ptr_,1);
-                    JSONCONS_RETHROW;
-                }
-            }
-
             array_storage(pointer ptr, semantic_tag tag)
                 : storage_kind_(static_cast<uint8_t>(json_storage_kind::array)), short_str_length_(0), tag_(tag), ptr_(ptr)
             {
-            }
-
-            array_storage(const array& val, semantic_tag tag)
-                : storage_kind_(static_cast<uint8_t>(json_storage_kind::array)), short_str_length_(0), tag_(tag), ptr_(nullptr)
-            {
-                create(val.get_allocator(), val);
-            }
-
-            array_storage(array&& val, semantic_tag tag)
-                : storage_kind_(static_cast<uint8_t>(json_storage_kind::array)), short_str_length_(0), tag_(tag), ptr_(nullptr)
-            {
-                create(val.get_allocator(), std::move(val));
             }
 
             array_storage(const array_storage& other)
@@ -2900,7 +2872,8 @@ namespace jsoncons {
                    semantic_tag tag = semantic_tag::none, 
                    const Allocator& alloc = Allocator()) 
         {
-            construct<array_storage>(array(first,last,alloc), tag);
+            auto ptr = create_array(alloc, first, last);
+            construct<array_storage>(ptr, tag);
         }
 
         basic_json(json_array_arg_t, 
@@ -2908,7 +2881,8 @@ namespace jsoncons {
                    semantic_tag tag = semantic_tag::none, 
                    const Allocator& alloc = Allocator()) 
         {
-            construct<array_storage>(array(init,alloc), tag);
+            auto ptr = create_array(alloc, init);
+            construct<array_storage>(ptr, tag);
         }
 
         basic_json(json_const_pointer_arg_t, const basic_json* p) noexcept 
@@ -2925,12 +2899,14 @@ namespace jsoncons {
 
         basic_json(const array& val, semantic_tag tag = semantic_tag::none)
         {
-            construct<array_storage>(val, tag);
+            auto ptr = create_array(val.get_allocator(), val);
+            construct<array_storage>(ptr, tag);
         }
 
         basic_json(array&& val, semantic_tag tag = semantic_tag::none)
         {
-            construct<array_storage>(std::move(val), tag);
+            auto ptr = create_array(val.get_allocator(), std::move(val));
+            construct<array_storage>(ptr, tag);
         }
 
         basic_json(const object& val, semantic_tag tag = semantic_tag::none)
