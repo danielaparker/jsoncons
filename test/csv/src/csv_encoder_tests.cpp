997092ae1a8db3d3ebb3f41724b6c4480c4411fc
Daniel Parker <danielaparker@yahoo.com>
2025-01-19 18:53:21 -0500

    csv_options column_mapping

--- test/csv/src/csv_encoder_tests.cpp
+++ test/csv/src/csv_encoder_tests.cpp
@@ -7,6 +7,68 @@
 
 namespace csv = jsoncons::csv; 
 
+TEST_CASE("test json to flat csv with column mappings")
+{
+//#if 0
+    SECTION("array of objects to csv")
+    {
+        std::string expected = R"(Number,Date Time
+1.0,1971-01-01T04:14:00
+1.27,1948-01-01T14:57:13
+)";
+
+        std::string jtext = R"(
+[
+    {
+        "text": "Chicago Reader", 
+        "float": 1.0, 
+        "datetime": "1971-01-01T04:14:00", 
+        "boolean": true,
+        "nested": {
+          "time": "04:14:00",
+          "nested": {
+            "date": "1971-01-01",
+            "integer": 40
+          }
+        }
+    }, 
+    {
+        "text": "Chicago Sun-Times", 
+        "float": 1.27, 
+        "datetime": "1948-01-01T14:57:13", 
+        "boolean": true,
+        "nested": {
+          "time": "14:57:13",
+          "nested": {
+            "date": "1948-01-01",
+            "integer": 63
+          }
+        }
+    }
+]
+        )";
+
+        
+        auto j = jsoncons::json::parse(jtext);
+        //std::cout << pretty_print(j) << "\n";
+
+        auto options = csv::csv_options{}
+            .flat(true)
+            .column_mapping({ 
+                {"/float","Number"},
+                {"/datetime","Date Time"} 
+                });
+
+        std::string buf;
+        csv::csv_string_encoder encoder(buf, options);
+        j.dump(encoder);
+        
+        //std::cout << buf << "\n";
+        
+        CHECK(expected == buf);
+    }
+}
+
 TEST_CASE("test json to flat csv")
 {
 #if 0
