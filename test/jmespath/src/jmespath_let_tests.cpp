620f2e97c364a2ef82f2838ce1f8a217c6f7e34f
Daniel Parker <danielaparker@yahoo.com>
2025-02-20 22:11:31 -0500

    jmespath let expr

--- test/jmespath/src/jmespath_let_tests.cpp
+++ test/jmespath/src/jmespath_let_tests.cpp
@@ -26,5 +26,16 @@ TEST_CASE("jmespath let tests")
         jsoncons::json result = expr.evaluate(doc);
         CHECK(expected == result);
     }    
+    SECTION("Test 2")
+    {
+        auto doc = jsoncons::json::parse(R"({"foo": {"bar": "baz"}})");
+        auto expected = jsoncons::json::parse(R"("baz")");
+
+        std::string query = R"(let $foo = foo.bar in $foo)";
+        auto expr = jmespath::make_expression<jsoncons::json>(query);
+
+        jsoncons::json result = expr.evaluate(doc);
+        CHECK(expected == result);
+    }    
 }
 
