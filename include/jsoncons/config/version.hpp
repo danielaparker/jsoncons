82aba941ee084dec5e77053539f4570e3c5e375b
Daniel Parker <danielaparker@yahoo.com>
2025-04-09 00:01:31 -0400

    For Release 1.3.1

--- include/jsoncons/config/version.hpp
+++ include/jsoncons/config/version.hpp
@@ -10,8 +10,8 @@
 #include <iostream>
     
 #define JSONCONS_VERSION_MAJOR 1
-#define JSONCONS_VERSION_MINOR 4
-#define JSONCONS_VERSION_PATCH 0
+#define JSONCONS_VERSION_MINOR 3
+#define JSONCONS_VERSION_PATCH 1
 
 namespace jsoncons {
 
