854726616b795f45fd021e6c4af6a892716f2250
Daniel Parker <danielaparker@yahoo.com>
2025-11-17 12:50:54 -0500

    Address alert in test code

--- test/corelib/src/json_in_place_update_tests.cpp
+++ test/corelib/src/json_in_place_update_tests.cpp
@@ -17,22 +17,21 @@ namespace {
         using jsoncons::default_json_visitor::string_view_type;
 
         my_in_place_updater(char* data, std::size_t length, 
-                            const std::string& from, const std::string& to)
+            const std::string& from, const std::string& to)
             : data_(data), length_(length), 
               from_(from), to_(to)
         {
-            (void)length_;
         }
         JSONCONS_VISITOR_RETURN_TYPE visit_string(const string_view_type& value,
-                          jsoncons::semantic_tag,
-                          const jsoncons::ser_context& context,
-                          std::error_code&) override
+            jsoncons::semantic_tag,
+            const jsoncons::ser_context& context,
+            std::error_code&) override
         {
             if (value == from_)
             {
-                //std::copy(to_.begin(), to_.end(), data_ + context.position() + 1);
                 auto p = data_ + context.position() + 1;
-                for (auto it = to_.begin(); it != to_.end(); ++it)
+                auto last = data_ + length_;
+                for (auto it = to_.begin(); it != to_.end() && p < last; ++it)
                 {
                     *p++ = *it;
                 }
@@ -74,7 +73,6 @@ TEST_CASE("json in-place update tests")
     }
     SECTION("test \r\n")
     {
-        //std::string format = "{\"items\"\r\n:\r\n \r\n[\r\n{\"id\"\r\n:\r\n1\r\n,\r\n \"name\" \r\n:\r\n \"abc\"\r\n,\r\n \"foo\" \r\n:\r\n 1000.0e-50\r\n,\r\n \"expiry\" \r\n:\r\n \"0420\"\r\n}\r\n\r\n,\r\n { \"id\"\r\n:\r\n2\r\n,\r\n\"name\" \r\n:\r\n \"%s\"\r\n,\r\n\"expiry\" \r\n:\r\n \"0720\" \r\n}\r\n\r\n]\r\n \r\n}";
         const std::string str1 = "{\"items\"\r\n:\r\n \r\n[\r\n{\"id\"\r\n:\r\n1\r\n,\r\n \"name\" \r\n:\r\n \"abc\"\r\n,\r\n \"foo\" \r\n:\r\n 1000.0e-50\r\n,\r\n \"expiry\" \r\n:\r\n \"0420\"\r\n}\r\n\r\n,\r\n { \"id\"\r\n:\r\n2\r\n,\r\n\"name\" \r\n:\r\n \"";
         const std::string str2 = "\"\r\n,\r\n\"expiry\" \r\n:\r\n \"0720\" \r\n}\r\n\r\n]\r\n \r\n}";
         std::string format = str1 + "%s" + str2;
@@ -89,7 +87,6 @@ TEST_CASE("json in-place update tests")
     }
     SECTION("test \r")
     {
-        //std::string format = "{\"items\"\r:\r \r[\r{\"id\"\r:\r1\r,\r \"name\" \r:\r \"abc\"\r,\r \"expiry\" \r:\r \"0420\"\r}\r\r,\r { \"id\"\r:\r2\r,\r\"name\" \r:\r \"%s\"\r,\r\"expiry\" \r:\r \"0720\" \r}\r\r]\r \r}";
         const std::string str1 = "{\"items\"\r:\r \r[\r{\"id\"\r:\r1\r,\r \"name\" \r:\r \"abc\"\r,\r \"expiry\" \r:\r \"0420\"\r}\r\r,\r { \"id\"\r:\r2\r,\r\"name\" \r:\r \"";
         const std::string str2 = "\"\r,\r\"expiry\" \r:\r \"0720\" \r}\r\r]\r \r}";
         std::string format = str1 + "%s" + str2;
@@ -104,7 +101,6 @@ TEST_CASE("json in-place update tests")
     }
     SECTION("test /**/ \n")
     {
-        //std::string format = "/*\n       \n        \n*/\"%s\"";
         const std::string str1 = "/*\n       \n        \n*/\"";
         const std::string str2 = "\"";
         std::string format = str1 + "%s" + str2;
@@ -119,7 +115,6 @@ TEST_CASE("json in-place update tests")
     }
     SECTION("test /**/ \r")
     {
-        //std::string format = "/*\r       \r        \r*/\"%s\"";
         const std::string str1 = "/*\r       \r        \r*/\"";
         const std::string str2 = "\"";
         std::string format = str1 + "%s" + str2;
@@ -134,7 +129,6 @@ TEST_CASE("json in-place update tests")
     }
     SECTION("test /**/ \r\n")
     {
-        //std::string format = "/*\r\n       \r\n        \r\n*/\"%s\"";
         const std::string str1 = "/*\r\n       \r\n        \r\n*/\"";
         const std::string str2 = "\"";
         std::string format = str1 + "%s" + str2;
@@ -149,7 +143,6 @@ TEST_CASE("json in-place update tests")
     }
     SECTION("test // \n")
     {
-        //std::string format = "//               \n\"%s\"";
         const std::string str1 = "//               \n\"";
         const std::string str2 = "\"";
         std::string format = str1 + "%s" + str2;
@@ -164,7 +157,6 @@ TEST_CASE("json in-place update tests")
     }
     SECTION("test // \r")
     {
-        //std::string format = "//               \r\"%s\"";
         const std::string str1 = "//               \r\"";
         const std::string str2 = "\"";
         std::string format = str1 + "%s" + str2;
@@ -179,7 +171,6 @@ TEST_CASE("json in-place update tests")
     }
     SECTION("test // \r\n")
     {
-        //std::string format = "//               \r\n\"%s\"";
         const std::string str1 = "//               \r\n\"";
         const std::string str2 = "\"";
         std::string format = str1 + "%s" + str2;
