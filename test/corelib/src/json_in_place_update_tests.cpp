fdee97e479dad28eaa7dddda1712595d9a0d108d
Daniel Parker <danielaparker@yahoo.com>
2025-11-17 11:40:55 -0500

    Remove snprintf from tests

--- test/corelib/src/json_in_place_update_tests.cpp
+++ test/corelib/src/json_in_place_update_tests.cpp
@@ -2,12 +2,7 @@
 // Distributed under Boost license
 
 #include <jsoncons/json.hpp>
-#include <jsoncons/json_encoder.hpp>
-#include <sstream>
-#include <vector>
-#include <utility>
-#include <ctime>
-#include <map>
+#include <string>
 #include <catch/catch.hpp>
 
 namespace {
@@ -52,143 +47,147 @@ TEST_CASE("json in-place update tests")
 {
     SECTION("test compact\n")
     {
-        std::string format = "{\"items\": [{\"id\":1, \"name\" : \"abc\", \"expiry\" : \"0420\"}, { \"id\":2,\"name\" : \"%s\",\"expiry\" : \"0720\" }] }";
+        const std::string str1 = R"({"items": [{"id":1, "name" : "abc", "expiry" : "0420"}, { "id":2,"name" : ")";
+        const std::string str2 = R"(","expiry" : "0720" }] })";
 
-        char input[500];
-        int length = snprintf(input, 500, format.c_str(), "id");
-        //std::cout << "(1)\n" << input << "\n";
-        char expected[500];
-        snprintf(expected, 500, format.c_str(), "ab");
+        std::string input = str1 + "id" + str2;
+        std::string expected = str1 + "ab" + str2;
 
-        my_in_place_updater updater(input, (size_t)length, "id", "ab");
+        my_in_place_updater updater(&input[0], input.size(), "id", "ab");
         jsoncons::json_string_reader reader(input, updater);
         reader.read();
         CHECK(std::string(input) == std::string(expected));
     }
     SECTION("test \n")
     {
-        std::string format = "{\"items\"\n:\n \n[\n{\"id\"\n:\n1\n,\n \"name\" \n:\n \"abc\"\n,\n \"expiry\" \n:\n \"0420\"\n}\n\n,\n { \"id\"\n:\n2\n,\n\"name\" \n:\n \"%s\"\n,\n\"expiry\" \n:\n \"0720\" \n}\n\n]\n \n}";
+        const std::string str1 = "{\"items\"\n:\n \n[\n{\"id\"\n:\n1\n,\n \"name\" \n:\n \"abc\"\n,\n \"expiry\" \n:\n \"0420\"\n}\n\n,\n { \"id\"\n:\n2\n,\n\"name\" \n:\n \"";
+        const std::string str2 = "\"\n,\n\"expiry\" \n:\n \"0720\" \n}\n\n]\n \n}";
+        std::string format = str1 + "%s" + str2;
 
-        char input[500];
-        int length = snprintf(input, 500, format.c_str(), "id");
-        //std::cout << "(1)\n" << input << "\n";
-        char expected[500];
-        snprintf(expected, 500, format.c_str(), "ab");
+        std::string input = str1 + "id" + str2;
+        std::string expected = str1 + "ab" + str2;
 
-        my_in_place_updater updater(input, (size_t)length, "id", "ab");
+        my_in_place_updater updater(&input[0], input.size(), "id", "ab");
         jsoncons::json_string_reader reader(input, updater);
         reader.read();
         CHECK(std::string(input) == std::string(expected));
     }
     SECTION("test \r\n")
     {
-        std::string format = "{\"items\"\r\n:\r\n \r\n[\r\n{\"id\"\r\n:\r\n1\r\n,\r\n \"name\" \r\n:\r\n \"abc\"\r\n,\r\n \"foo\" \r\n:\r\n 1000.0e-50\r\n,\r\n \"expiry\" \r\n:\r\n \"0420\"\r\n}\r\n\r\n,\r\n { \"id\"\r\n:\r\n2\r\n,\r\n\"name\" \r\n:\r\n \"%s\"\r\n,\r\n\"expiry\" \r\n:\r\n \"0720\" \r\n}\r\n\r\n]\r\n \r\n}";
+        //std::string format = "{\"items\"\r\n:\r\n \r\n[\r\n{\"id\"\r\n:\r\n1\r\n,\r\n \"name\" \r\n:\r\n \"abc\"\r\n,\r\n \"foo\" \r\n:\r\n 1000.0e-50\r\n,\r\n \"expiry\" \r\n:\r\n \"0420\"\r\n}\r\n\r\n,\r\n { \"id\"\r\n:\r\n2\r\n,\r\n\"name\" \r\n:\r\n \"%s\"\r\n,\r\n\"expiry\" \r\n:\r\n \"0720\" \r\n}\r\n\r\n]\r\n \r\n}";
+        const std::string str1 = "{\"items\"\r\n:\r\n \r\n[\r\n{\"id\"\r\n:\r\n1\r\n,\r\n \"name\" \r\n:\r\n \"abc\"\r\n,\r\n \"foo\" \r\n:\r\n 1000.0e-50\r\n,\r\n \"expiry\" \r\n:\r\n \"0420\"\r\n}\r\n\r\n,\r\n { \"id\"\r\n:\r\n2\r\n,\r\n\"name\" \r\n:\r\n \"";
+        const std::string str2 = "\"\r\n,\r\n\"expiry\" \r\n:\r\n \"0720\" \r\n}\r\n\r\n]\r\n \r\n}";
+        std::string format = str1 + "%s" + str2;
 
-        char input[500];
-        int length = snprintf(input, 500, format.c_str(), "id");
-        char expected[500];
-        snprintf(expected, 500, format.c_str(), "ab");
+        std::string input = str1 + "id" + str2;
+        std::string expected = str1 + "ab" + str2;
 
-        my_in_place_updater updater(input, (size_t)length, "id", "ab");
+        my_in_place_updater updater(&input[0], input.size(), "id", "ab");
         jsoncons::json_string_reader reader(input, updater);
         reader.read();
         CHECK(std::string(input) == std::string(expected));
     }
     SECTION("test \r")
     {
-        std::string format = "{\"items\"\r:\r \r[\r{\"id\"\r:\r1\r,\r \"name\" \r:\r \"abc\"\r,\r \"expiry\" \r:\r \"0420\"\r}\r\r,\r { \"id\"\r:\r2\r,\r\"name\" \r:\r \"%s\"\r,\r\"expiry\" \r:\r \"0720\" \r}\r\r]\r \r}";
+        //std::string format = "{\"items\"\r:\r \r[\r{\"id\"\r:\r1\r,\r \"name\" \r:\r \"abc\"\r,\r \"expiry\" \r:\r \"0420\"\r}\r\r,\r { \"id\"\r:\r2\r,\r\"name\" \r:\r \"%s\"\r,\r\"expiry\" \r:\r \"0720\" \r}\r\r]\r \r}";
+        const std::string str1 = "{\"items\"\r:\r \r[\r{\"id\"\r:\r1\r,\r \"name\" \r:\r \"abc\"\r,\r \"expiry\" \r:\r \"0420\"\r}\r\r,\r { \"id\"\r:\r2\r,\r\"name\" \r:\r \"";
+        const std::string str2 = "\"\r,\r\"expiry\" \r:\r \"0720\" \r}\r\r]\r \r}";
+        std::string format = str1 + "%s" + str2;
 
-        char input[500];
-        int length = snprintf(input, 500, format.c_str(), "id");
+        std::string input = str1 + "id" + str2;
+        std::string expected = str1 + "ab" + str2;
 
-        char expected[500];
-        snprintf(expected, 500, format.c_str(), "ab");
-
-        my_in_place_updater updater(input, (size_t)length, "id", "ab");
+        my_in_place_updater updater(&input[0], input.size(), "id", "ab");
         jsoncons::json_string_reader reader(input, updater);
         reader.read();
         CHECK(std::string(input) == std::string(expected));
     }
     SECTION("test /**/ \n")
     {
-        std::string format = "/*\n       \n        \n*/\"%s\"";
+        //std::string format = "/*\n       \n        \n*/\"%s\"";
+        const std::string str1 = "/*\n       \n        \n*/\"";
+        const std::string str2 = "\"";
+        std::string format = str1 + "%s" + str2;
 
-        char input[500];
-        int length = snprintf(input, 500, format.c_str(), "id");
-        char expected[500];
-        snprintf(expected, 500, format.c_str(), "ab");
+        std::string input = str1 + "id" + str2;
+        std::string expected = str1 + "ab" + str2;
 
-        my_in_place_updater updater(input, (size_t)length, "id", "ab");
+        my_in_place_updater updater(&input[0], input.size(), "id", "ab");
         jsoncons::json_string_reader reader(input, updater);
         reader.read();
         CHECK(std::string(input) == std::string(expected));
     }
     SECTION("test /**/ \r")
     {
-        std::string format = "/*\r       \r        \r*/\"%s\"";
+        //std::string format = "/*\r       \r        \r*/\"%s\"";
+        const std::string str1 = "/*\r       \r        \r*/\"";
+        const std::string str2 = "\"";
+        std::string format = str1 + "%s" + str2;
 
-        char input[500];
-        int length = snprintf(input, 500, format.c_str(), "id");
-        char expected[500];
-        snprintf(expected, 500, format.c_str(), "ab");
+        std::string input = str1 + "id" + str2;
+        std::string expected = str1 + "ab" + str2;
 
-        my_in_place_updater updater(input, (size_t)length, "id", "ab");
+        my_in_place_updater updater(&input[0], input.size(), "id", "ab");
         jsoncons::json_string_reader reader(input, updater);
         reader.read();
         CHECK(std::string(input) == std::string(expected));
     }
     SECTION("test /**/ \r\n")
     {
-        std::string format = "/*\r\n       \r\n        \r\n*/\"%s\"";
+        //std::string format = "/*\r\n       \r\n        \r\n*/\"%s\"";
+        const std::string str1 = "/*\r\n       \r\n        \r\n*/\"";
+        const std::string str2 = "\"";
+        std::string format = str1 + "%s" + str2;
 
-        char input[500];
-        int length = snprintf(input, 500, format.c_str(), "id");
-        char expected[500];
-        snprintf(expected, 500, format.c_str(), "ab");
+        std::string input = str1 + "id" + str2;
+        std::string expected = str1 + "ab" + str2;
 
-        my_in_place_updater updater(input, (size_t)length, "id", "ab");
+        my_in_place_updater updater(&input[0], input.size(), "id", "ab");
         jsoncons::json_string_reader reader(input, updater);
         reader.read();
         CHECK(std::string(input) == std::string(expected));
     }
     SECTION("test // \n")
     {
-        std::string format = "//               \n\"%s\"";
+        //std::string format = "//               \n\"%s\"";
+        const std::string str1 = "//               \n\"";
+        const std::string str2 = "\"";
+        std::string format = str1 + "%s" + str2;
 
-        char input[500];
-        int length = snprintf(input, 500, format.c_str(), "id");
-        char expected[500];
-        snprintf(expected, 500, format.c_str(), "ab");
+        std::string input = str1 + "id" + str2;
+        std::string expected = str1 + "ab" + str2;
 
-        my_in_place_updater updater(input, (size_t)length, "id", "ab");
+        my_in_place_updater updater(&input[0], input.size(), "id", "ab");
         jsoncons::json_string_reader reader(input, updater);
         reader.read();
         CHECK(std::string(input) == std::string(expected));
     }
     SECTION("test // \r")
     {
-        std::string format = "//               \r\"%s\"";
+        //std::string format = "//               \r\"%s\"";
+        const std::string str1 = "//               \r\"";
+        const std::string str2 = "\"";
+        std::string format = str1 + "%s" + str2;
 
-        char input[500];
-        int length = snprintf(input, 500, format.c_str(), "id");
-        char expected[500];
-        snprintf(expected, 500, format.c_str(), "ab");
+        std::string input = str1 + "id" + str2;
+        std::string expected = str1 + "ab" + str2;
 
-        my_in_place_updater updater(input, (size_t)length, "id", "ab");
+        my_in_place_updater updater(&input[0], input.size(), "id", "ab");
         jsoncons::json_string_reader reader(input, updater);
         reader.read();
         CHECK(std::string(input) == std::string(expected));
     }
     SECTION("test // \r\n")
     {
-        std::string format = "//               \r\n\"%s\"";
+        //std::string format = "//               \r\n\"%s\"";
+        const std::string str1 = "//               \r\n\"";
+        const std::string str2 = "\"";
+        std::string format = str1 + "%s" + str2;
 
-        char input[500];
-        int length = snprintf(input, 500, format.c_str(), "id");
-        char expected[500];
-        snprintf(expected, 500, format.c_str(), "ab");
+        std::string input = str1 + "id" + str2;
+        std::string expected = str1 + "ab" + str2;
 
-        my_in_place_updater updater(input, (size_t)length, "id", "ab");
+        my_in_place_updater updater(&input[0], input.size(), "id", "ab");
         jsoncons::json_string_reader reader(input, updater);
         reader.read();
         CHECK(std::string(input) == std::string(expected));
