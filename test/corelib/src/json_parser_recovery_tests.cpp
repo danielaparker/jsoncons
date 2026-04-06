15a930b31c8fc861d3e832a54dbe2704185c26d1
Daniel Parker <danielaparker@yahoo.com>
2026-04-06 18:43:10 -0400

    Merge branch 'master' of https://github.com/danielaparker/jsoncons into json_tokenizer_cursor
    # Conflicts:
    #	include/jsoncons/json_parser.hpp
    #	test/corelib/src/json_parser_recovery_tests.cpp

--- test/corelib/src/json_parser_recovery_tests.cpp
+++ test/corelib/src/json_parser_recovery_tests.cpp
@@ -39,30 +39,6 @@ TEST_CASE("test_array_extra_comma")
 
 TEST_CASE("test_object_extra_comma")
 {
-#if !defined(JSONCONS_NO_DEPRECATED)
-    SECTION("using err_handler")
-    {
-        allow_trailing_commas err_handler;
-
-        json expected = json::parse(R"(
-    {
-        "first" : 1,
-        "second" : 2
-    }
-    )",
-            err_handler);
-
-        json val = json::parse(R"(
-    {
-        "first" : 1,
-        "second" : 2,
-    }
-    )",
-            err_handler);
-
-        CHECK(expected == val);
-    }
-#endif
     SECTION("with option")
     {
         auto options = json_options{}
@@ -84,72 +60,6 @@ TEST_CASE("test_object_extra_comma")
 
         CHECK(expected == val);
     }
-}
-
-TEST_CASE("test json_parser error recovery")
-{
-#if !defined(JSONCONS_NO_DEPRECATED)
-    SECTION("illegal control character")
-    {
-        auto err_handler = [](const std::error_code& ec, const ser_context&) noexcept -> bool
-            {
-                return ec == json_errc::illegal_control_character;
-            };
-        
-        std::string str;
-        str.push_back('"');
-        str.push_back('C');
-        str.push_back(0x0e);
-        str.push_back('a');
-        str.push_back('t');
-        str.push_back('"');
-        auto j = jsoncons::json::parse(str, err_handler);
-        REQUIRE(j.is_string());
-        CHECK(j.as_string() == "Cat");
-    }
-    SECTION("\r")
-    {
-        auto err_handler = [](const std::error_code& ec, const ser_context&) noexcept -> bool
-            {
-                return ec == json_errc::illegal_character_in_string;
-            };
-
-
-        std::string str;
-        str.push_back('C');
-        str.push_back('\r');
-        str.push_back('a');
-        str.push_back('t');
-        std::string str2;
-        str2.push_back('"');
-        str2.append(str);
-        str2.push_back('"');
-        auto j = jsoncons::json::parse(str2, err_handler);
-        REQUIRE(j.is_string());
-        CHECK(j.as_string() == "Cat");
-    }
-    SECTION("\n")
-    {
-        auto err_handler = [](const std::error_code& ec, const ser_context&) noexcept -> bool
-            {
-                return ec == json_errc::illegal_character_in_string;
-            };
-
-
-        std::string str;
-        str.push_back('C');
-        str.push_back('\n');
-        str.push_back('a');
-        str.push_back('t');
-        std::string str2;
-        str2.push_back('"');
-        str2.append(str);
-        str2.push_back('"');
-        auto j = jsoncons::json::parse(str2, err_handler);
-        REQUIRE(j.is_string());
-        CHECK(j.as_string() == "Cat");
-    }
-#endif
 }
 
 
