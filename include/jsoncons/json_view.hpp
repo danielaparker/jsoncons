fff5fe6db6cd05198ea058ec773bd6aa1b26ff8d
Daniel Parker <danielaparker@yahoo.com>
2026-05-06 18:44:44 -0400

    json_view

--- include/jsoncons/json_view.hpp
+++ include/jsoncons/json_view.hpp
@@ -17,10 +17,15 @@ namespace jsoncons {
 
 enum class json_view_storage_kind : uint8_t {json_ref,array,object};
 
-template <typename Json>
+template <typename Json,typename Allocator=std::allocator<char>>
 class json_view
 {
+    using allocator_type = Allocator;
     using json_type = typename std::remove_const<Json>::type;
+    using key_type = typename json_type::key_type;
+    using policy_type = typename json_type::policy_type;
+    using array = typename policy_type::template array<json_view>;
+    using object = typename policy_type::template object<key_type,json_view>;
 
     struct common_storage
     {
@@ -38,10 +43,60 @@ class json_view
         {
         }
     };
+
+    struct array_storage
+    {
+        using allocator_type = typename std::allocator_traits<Allocator>:: template rebind_alloc<array>;
+        using pointer = typename std::allocator_traits<allocator_type>::pointer;
+
+       json_view_storage_kind storage_kind_;
+        pointer ptr_;
+
+        array_storage(pointer ptr)
+            : storage_kind_(static_cast<uint8_t>(json_view_storage_kind::array)), ptr_(ptr)
+        {
+        }
+
+        array_storage(const array_storage& other)
+            : storage_kind_(other.storage_kind_), ptr_(other.ptr_)
+        {
+        }
+
+        Allocator get_allocator() const
+        {
+            return ptr_->get_allocator();
+        }
+    };
+    struct object_storage
+    {
+        using allocator_type = typename std::allocator_traits<Allocator>:: template rebind_alloc<object>;
+        using pointer = typename std::allocator_traits<allocator_type>::pointer;
+
+        json_view_storage_kind storage_kind_;
+        pointer ptr_;
+
+        object_storage(pointer ptr)
+            : storage_kind_(static_cast<uint8_t>(json_view_storage_kind::object)), ptr_(ptr)
+        {
+        }
+
+        explicit object_storage(const object_storage& other)
+            : storage_kind_(other.storage_kind_), ptr_(other.ptr_)
+        {
+        }
+
+        Allocator get_allocator() const
+        {
+            JSONCONS_ASSERT(ptr_ != nullptr);
+            return ptr_->get_allocator();
+        }
+    };
     union 
     {
         common_storage common_;
         json_ref_storage json_ref_;
+        array_storage array_;
+        object_storage object_;
     };
 public:
     json_view(Json& j)
