583625defc4c90a320439ebf41a9f7292f702078
Daniel Parker <danielaparker@yahoo.com>
2026-09-11 16:35:51 -0400

    bigdec

---
+++ include/jsoncons/utility/bigdec.hpp
@@ -0,0 +1,14 @@
+// Copyright 2018 Daniel Parker
+// Distributed under the Boost license, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+// See https://github.com/danielaparker/jsoncons for latest version
+
+#ifndef JSONCONS_UTILITY_BIGDEC_HPP
+#define JSONCONS_UTILITY_BIGDEC_HPP
+
+namespace jsoncons {
+
+} // namespace jsoncons
+
+#endif // JSONCONS_UTILITY_BIGDEC_HPP
---
+++ test/corelib/src/utility/bigdec_tests.cpp
@@ -0,0 +1,16 @@
+// Copyright 2013-2026 Daniel Parker
+// Distributed under Boost license
+
+#if defined(_MSC_VER)
+#include "windows.h"
+#endif
+#include <jsoncons/utility/bigdec.hpp>
+#include <catch/catch.hpp>
+
+TEST_CASE("basic_bigdec tests")
+{
+    SECTION("0")
+    {
+    }
+}
+
